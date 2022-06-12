5ace095a2f16fa38e20484e6800915fac94cbc6e
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-12 16:55:26 +0300

    Add popAfterDelete parameter to DocDeleteButton
    and use SingleDocName

--- lib/components/doc_delete_button.dart
+++ lib/components/doc_delete_button.dart
@@ -3,19 +3,22 @@ import 'package:soar_quest/data/sq_doc.dart';
 
 class DocDeleteButton extends StatelessWidget {
   final SQDoc doc;
+  final bool popAfterDelete;
 
-  const DocDeleteButton(this.doc, {Key? key}) : super(key: key);
+  const DocDeleteButton(this.doc, {this.popAfterDelete = true, Key? key})
+      : super(key: key);
 
   void deleteDoc(BuildContext context) {
     doc.collection.deleteDoc(doc.id).then((_) {
       doc.collection.refreshUI!();
-      Navigator.pop(context);
+      if (popAfterDelete) Navigator.pop(context);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () => deleteDoc(context), child: Text("Delete Doc"));
+        onPressed: () => deleteDoc(context),
+        child: Text("Delete ${doc.collection.singleDocName}"));
   }
 }
