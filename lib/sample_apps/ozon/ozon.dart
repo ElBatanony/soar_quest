e84239f0adf629b7b066c3fbb8f8a0023c2ae731
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-12 08:33:47 +0300

    Save intermediate Ozon app state

--- lib/sample_apps/ozon/ozon.dart
+++ lib/sample_apps/ozon/ozon.dart
@@ -4,11 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:soar_quest/apps/app.dart';
 import 'package:soar_quest/apps/app_display.dart';
-import 'package:soar_quest/data_objects/data_collection.dart';
-import 'package:soar_quest/data_objects/data_object.dart';
+import 'package:soar_quest/data/sq_collection.dart';
+import 'package:soar_quest/data/sq_doc.dart';
 import 'package:soar_quest/firebase_options.dart';
-import 'package:soar_quest/screens/collection_display_screen.dart';
-import 'package:soar_quest/screens/data_display_screen.dart';
+import 'package:soar_quest/screens/collection_screen.dart';
+import 'package:soar_quest/screens/doc_screen.dart';
+import 'package:soar_quest/screens/main_screen.dart';
 import 'package:soar_quest/screens/menu_screen.dart';
 import 'package:soar_quest/screens/screen.dart';
 import 'package:soar_quest/users/user_data.dart';
@@ -17,10 +18,38 @@ void main() async {
   App ozonApp = App("Ozon");
   App.instance.currentUser = UserData(userId: "testuser123");
 
-  // final Navigation
-  // final MenuScreen homescreen = MenuScreen("Tinkoff Homescreen",
-  //     [Screen("Profile"), Screen('Payments')]);
-  // tinkoffApp.homescreen = homescreen;
+  final catalogueCollection = SQCollection([
+    SQDocField("Item Name", SQDocFieldType.string),
+    SQDocField("Item Price", SQDocFieldType.int),
+  ], "catalogue");
+
+  Widget catalogueObjectDisplayBody(SQDoc object) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
+      Text("this is the custom diplay of the ozon catalogue item"),
+      ElevatedButton(onPressed: null, child: Text("add to favourites")),
+      Text("custom display")
+    ]
+        // object.fields.map((field) => DataFieldDisplay(field)).toList()
+        );
+  }
+
+  final catalogueScreen = CollectionScreen(
+    "Catalogue",
+    catalogueCollection,
+    dataObjectDisplayBody: catalogueObjectDisplayBody,
+  );
+
+  final MainScreen homescreen = MainScreen(
+    [
+      Screen("Main"),
+      Screen('Fresh'),
+      catalogueScreen,
+      Screen('Cart'),
+      Screen('Favourites')
+    ],
+    initialScreenIndex: 2,
+  );
+  ozonApp.homescreen = homescreen;
 
   ozonApp.run();
 }
