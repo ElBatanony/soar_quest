4d3a535fe7d5823c6b24b1ee59eb2eba94953006
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-12 13:59:53 +0300

    Use App.run in tinkoff and youtube

--- lib/sample_apps/tinkoff/tinkoff.dart
+++ lib/sample_apps/tinkoff/tinkoff.dart
@@ -1,12 +1,8 @@
 // ignore_for_file: unused_local_variable
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:soar_quest/app/app.dart';
-import 'package:soar_quest/app/app_display.dart';
 import 'package:soar_quest/data/sq_collection.dart';
 import 'package:soar_quest/data/sq_doc.dart';
-import 'package:soar_quest/firebase_options.dart';
 import 'package:soar_quest/screens/collection_screen.dart';
 import 'package:soar_quest/screens/doc_screen.dart';
 import 'package:soar_quest/screens/menu_screen.dart';
@@ -51,10 +47,6 @@ void main() async {
   final MenuScreen homescreen = MenuScreen("Tinkoff Homescreen",
       [Screen("Profile"), Screen('Payments'), cashbackBonusesScreen]);
   tinkoffApp.homescreen = homescreen;
-  // tinkoffApp.homescreen = partnerCashbackScreen;
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(AppDisplay(tinkoffApp));
+  tinkoffApp.run();
 }
--- lib/sample_apps/youtube/youtube.dart
+++ lib/sample_apps/youtube/youtube.dart
@@ -1,12 +1,8 @@
 // ignore_for_file: unused_local_variable
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:soar_quest/app/app.dart';
-import 'package:soar_quest/app/app_display.dart';
 import 'package:soar_quest/data/sq_collection.dart';
 import 'package:soar_quest/data/sq_doc.dart';
-import 'package:soar_quest/firebase_options.dart';
 import 'package:soar_quest/screens/collection_screen.dart';
 import 'package:soar_quest/screens/menu_screen.dart';
 import 'package:soar_quest/screens/screen.dart';
@@ -41,8 +37,5 @@ void main() async {
 
   youtubeApp.homescreen = homescreen;
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(AppDisplay(youtubeApp));
+  youtubeApp.run();
 }
