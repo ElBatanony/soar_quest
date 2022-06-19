a4da4863ad439d54feef97e5de9055245b6d0ef4
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-19 07:18:10 +0300

    Show Sign Out button in ScreenScaffold

--- lib/screens/screen_scaffold.dart
+++ lib/screens/screen_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:soar_quest/screens/screen.dart';
+import 'package:soar_quest/users/auth_manager.dart';
 
 class ScreenScaffold extends StatelessWidget {
   final String title;
@@ -10,8 +12,50 @@ class ScreenScaffold extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: [
+          ShowIfAuthState(
+              child: IconButton(
+                  onPressed: AuthManager.signOut, icon: Icon(Icons.logout)))
+        ],
       ),
       body: Center(child: child),
+    );
+  }
+}
+
+class ShowIfAuthState extends StatelessWidget {
+  final bool showIfSignedIn;
+  final Widget child;
+  const ShowIfAuthState(
+      {required this.child, this.showIfSignedIn = true, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Map<String, dynamic>>(
+        stream: AuthManager.authChanges,
+        builder: (context, snapshot) {
+          return snapshot.data?["signedIn"] == true ? child : Container();
+        });
+  }
+}
+
+class ScaffoldedScreen extends Screen {
+  final Widget screenBody;
+
+  ScaffoldedScreen(String title, {required this.screenBody, Key? key})
+      : super(title, key: key);
+
+  @override
+  State<ScaffoldedScreen> createState() => _ScaffoldedScreenState();
+}
+
+class _ScaffoldedScreenState extends State<ScaffoldedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenScaffold(
+      widget.title,
+      widget.screenBody,
     );
   }
 }
