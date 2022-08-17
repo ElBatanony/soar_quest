72f871651caa9e86a092c057a255dec375d0fa10
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-17 13:06:24 +0300

    Create SettingsScreen

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:soar_quest/app/app_settings.dart';
+import 'package:soar_quest/data/sq_doc.dart';
+
+import '../components/doc_field_field.dart';
+import 'doc_edit_screen.dart';
+import 'screen.dart';
+
+class SettingsScreen extends Screen {
+  SettingsScreen({String title = "Settings", Key? key})
+      : super(title, key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  SQDoc? settingsDoc;
+
+  loadSettings() async {
+    await AppSettings.init();
+    settingsDoc = AppSettings.settingsDoc;
+    setState(() {});
+  }
+
+  settingsUpdateCallback() {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Settings Updated"),
+    ));
+  }
+
+  @override
+  void initState() {
+    loadSettings();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: settingsDoc == null
+            ? Text("Loading settings")
+            : DocEditScreenBody(
+                settingsDoc!,
+                objectFieldsFields: settingsDoc!.fields
+                    .map((field) => DocFieldField(field))
+                    .toList(),
+                updateCallback: settingsUpdateCallback,
+              ));
+  }
+}
