b9873a0209633b0334f632996c85601533576820
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-17 13:11:53 +0300

    Update Tech Admin after SettingsScreen

--- lib/sample_apps/tech_admin/tech_admin.dart
+++ lib/sample_apps/tech_admin/tech_admin.dart
@@ -1,45 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:soar_quest/app/app.dart';
+import 'package:soar_quest/app/app_settings.dart';
 import 'package:soar_quest/data.dart';
 import 'package:soar_quest/data/firestore.dart';
-import 'package:soar_quest/screens/auth/sign_in_screen.dart';
 import 'package:soar_quest/screens/collection_screen.dart';
 import 'package:soar_quest/screens/main_screen.dart';
+import 'package:soar_quest/screens/playground_screen.dart';
 import 'package:soar_quest/screens/screen.dart';
-import 'package:soar_quest/screens/screen_scaffold.dart';
+import 'package:soar_quest/screens/settings_screen.dart';
 import 'package:soar_quest/users/auth_manager.dart';
 import 'package:soar_quest/users/user_data.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   App adminApp = App("Tech Admin",
-      theme: ThemeData(primarySwatch: Colors.amber, useMaterial3: true));
+      theme: ThemeData(primarySwatch: Colors.amber, useMaterial3: true),
+      inDebug: false);
+
+  await adminApp.init();
 
   App.instance.currentUser = UserData(userId: "testuser123");
 
   final logsCollection = FirestoreCollection(
       id: "Logs",
       fields: [
-        SQDocField("Task Name", SQDocFieldType.string),
-        SQDocField("Reminder", SQDocFieldType.string),
-        SQDocField("Done", SQDocFieldType.bool, value: false),
+        SQDocField("logId", SQDocFieldType.string),
+        SQDocField("message", SQDocFieldType.string),
+        SQDocField("date", SQDocFieldType.timestamp),
+        SQDocField("payload", SQDocFieldType.bool),
+        SQDocField("tags", SQDocFieldType.string),
       ],
       singleDocName: "Log");
 
   final logsScreen = CollectionScreen("Logs", logsCollection);
 
+  AppSettings.setSettings([
+    SQDocField('fawryCodeRequest', SQDocFieldType.bool),
+    SQDocField('userIdUploaded', SQDocFieldType.bool),
+    SQDocField('paymentError', SQDocFieldType.bool),
+    SQDocField('newUser', SQDocFieldType.bool),
+    SQDocField('payment', SQDocFieldType.bool),
+    SQDocField('manualMembership', SQDocFieldType.bool),
+    SQDocField('username', SQDocFieldType.string),
+    SQDocField('Log Manual Commands', SQDocFieldType.bool),
+  ]);
+
   adminApp.homescreen = MainScreen(
     [
       // AuthScreenTesting("Auth Testing"),
-      SignInScreen(
-        signedRedirectScreen: ScaffoldedScreen("Yay! You are signed in",
-            screenBody: Column(
-              children: [SignOutButton()],
-            )),
-      ),
-      Screen("Manage Users"),
+      // SignInScreen(
+      //   signedRedirectScreen: ScaffoldedScreen("Yay! You are signed in",
+      //       screenBody: Column(
+      //         children: [SignOutButton()],
+      //       )),
+      // ),
+      SettingsScreen(),
       logsScreen,
-      Screen("Settings"),
-      Screen("Profile")
+      Screen("Manage Users"),
+      PlaygroundScreen("Playground")
     ],
     initialScreenIndex: 0,
   );
