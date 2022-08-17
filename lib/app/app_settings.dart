99fbfe297c4586f8fe3d47b147e7c36f041ac787
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-17 13:03:33 +0300

    Create AppSettings

---
+++ lib/app/app_settings.dart
@@ -0,0 +1,21 @@
+import '../data.dart';
+import '../data/firestore.dart';
+
+class AppSettings {
+  static SQDoc settingsDoc = SQDoc(
+      'settings', [SQDocField('testSetting', SQDocFieldType.bool)],
+      collection: userCollection);
+
+  static void setSettings(List<SQDocField> settings) {
+    settingsDoc = SQDoc('settings', settings, collection: userCollection);
+  }
+
+  static Future init() async {
+    await settingsDoc.loadDoc();
+    return;
+  }
+
+  static getSetting(String settingsName) {
+    return settingsDoc.getFieldValueByName(settingsName);
+  }
+}
