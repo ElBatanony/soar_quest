8737fd67abd367d48331b8b6581d06a9e093f48b
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-19 07:48:06 +0300

    Create CollectionFilterScreen

---
+++ lib/screens/collection_filter_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:soar_quest/components/doc_field_field.dart';
+import 'package:soar_quest/data.dart';
+import 'package:soar_quest/data/docs_filter.dart';
+import 'collection_screen.dart';
+
+class CollectionFilterScreen extends CollectionScreen {
+  final List<DocsFilter> filters;
+
+  CollectionFilterScreen(String title,
+      {required SQCollection collection, required this.filters, Key? key})
+      : super(title, collection, key: key);
+
+  @override
+  State<CollectionFilterScreen> createState() => _CollectionFilterScreenState();
+}
+
+class _CollectionFilterScreenState extends State<CollectionFilterScreen> {
+  List<SQDoc> filteredDocs = [];
+
+  void loadData() async {
+    await widget.collection.loadCollection();
+    refreshScreen();
+    updateDocs();
+  }
+
+  void refreshScreen() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    widget.collection.refreshUI = refreshScreen;
+    loadData();
+
+    super.initState();
+  }
+
+  void updateDocs() {
+    filteredDocs = widget.collection.filter(widget.filters);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var itemsDisplay = filteredDocs
+        .map((doc) => CollectionScreenBodyDocButton(
+            doc, widget.collection, refreshScreen))
+        .toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("${widget.title} ${widget.collection.id}"),
+        ),
+        body: Column(
+          children: [
+            ...widget.filters
+                .map((filter) => DocFieldField(
+                      filter.field,
+                      onChanged: (value) => updateDocs(),
+                    ))
+                .toList(),
+            SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total docs: ${widget.collection.docs.length}',
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      'Showing docs: ${filteredDocs.length}',
+                      textAlign: TextAlign.center,
+                    ),
+                    ...itemsDisplay,
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
