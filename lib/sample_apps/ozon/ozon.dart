782ab7e43b83d58917fc07b903865500ded79e8c
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-19 11:58:21 +0300

    Comment out sample apps not in use

--- lib/sample_apps/ozon/ozon.dart
+++ lib/sample_apps/ozon/ozon.dart
@@ -1,69 +1,69 @@
-// ignore_for_file: unused_local_variable, unused_import
+// // ignore_for_file: unused_local_variable, unused_import
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:soar_quest/app/app.dart';
-import 'package:soar_quest/app/app_display.dart';
-import 'package:soar_quest/data/firestore.dart';
-import 'package:soar_quest/data/sq_collection.dart';
-import 'package:soar_quest/data/sq_doc.dart';
-import 'package:soar_quest/features/favourites/favourites.dart';
-import 'package:soar_quest/firebase_options.dart';
-import 'package:soar_quest/screens/collection_screen.dart';
-import 'package:soar_quest/screens/doc_screen.dart';
-import 'package:soar_quest/screens/main_screen.dart';
-import 'package:soar_quest/screens/menu_screen.dart';
-import 'package:soar_quest/screens/screen.dart';
-import 'package:soar_quest/users/user_data.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:soar_quest/app/app.dart';
+// import 'package:soar_quest/app/app_display.dart';
+// import 'package:soar_quest/data/firestore.dart';
+// import 'package:soar_quest/data/sq_collection.dart';
+// import 'package:soar_quest/data/sq_doc.dart';
+// import 'package:soar_quest/features/favourites/favourites.dart';
+// import 'package:soar_quest/firebase_options.dart';
+// import 'package:soar_quest/screens/collection_screen.dart';
+// import 'package:soar_quest/screens/doc_screen.dart';
+// import 'package:soar_quest/screens/main_screen.dart';
+// import 'package:soar_quest/screens/menu_screen.dart';
+// import 'package:soar_quest/screens/screen.dart';
+// import 'package:soar_quest/users/user_data.dart';
 
-void main() async {
-  App ozonApp = App("Ozon");
-  App.instance.currentUser = UserData(userId: "testuser123");
+// void main() async {
+//   App ozonApp = App("Ozon");
+//   App.instance.currentUser = UserData(userId: "testuser123");
 
-  final catalogueCollection = FirestoreCollection(id: "catalogue", fields: [
-    SQDocField("Item Name", SQDocFieldType.string),
-    SQDocField("Item Price", SQDocFieldType.int),
-  ]);
+//   final catalogueCollection = FirestoreCollection(id: "catalogue", fields: [
+//     SQDocField<String>("Item Name"),
+//     SQDocField<int>("Item Price"),
+//   ]);
 
-  final catalogueScreen = CollectionScreen(
-    "Catalogue",
-    catalogueCollection,
-    docScreenBody: CatalogueDocBody.new,
-  );
+//   final catalogueScreen = CollectionScreen(
+//     "Catalogue",
+//     catalogueCollection,
+//     docScreenBody: CatalogueDocBody.new,
+//   );
 
-  FavouritesFeature.loadFavourites();
+//   FavouritesFeature.loadFavourites();
 
-  final MainScreen homescreen = MainScreen(
-    [
-      Screen("Main"),
-      Screen('Fresh'),
-      catalogueScreen,
-      Screen('Cart'),
-      FavouritesFeature.favouritesScreen
-    ],
-    initialScreenIndex: 2,
-  );
-  ozonApp.homescreen = homescreen;
+//   final MainScreen homescreen = MainScreen(
+//     [
+//       Screen("Main"),
+//       Screen('Fresh'),
+//       catalogueScreen,
+//       Screen('Cart'),
+//       FavouritesFeature.favouritesScreen
+//     ],
+//     initialScreenIndex: 2,
+//   );
+//   ozonApp.homescreen = homescreen;
 
-  ozonApp.run();
-}
+//   ozonApp.run();
+// }
 
-class CatalogueDocBody extends DocScreenBody {
-  const CatalogueDocBody(SQDoc doc, {Key? key}) : super(doc, key: key);
+// class CatalogueDocBody extends DocScreenBody {
+//   const CatalogueDocBody(SQDoc doc, {Key? key}) : super(doc, key: key);
 
-  @override
-  State<CatalogueDocBody> createState() => _CataglogueDocBodyState();
-}
+//   @override
+//   State<CatalogueDocBody> createState() => _CataglogueDocBodyState();
+// }
 
-class _CataglogueDocBodyState extends State<CatalogueDocBody> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text("this is the custom diplay of the ozon catalogue item"),
-      Text("custom display"),
-      FavouritesFeature.addToFavouritesButton(widget.doc)
-    ]
-        // object.fields.map((field) => DataFieldDisplay(field)).toList()
-        );
-  }
-}
+// class _CataglogueDocBodyState extends State<CatalogueDocBody> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//       Text("this is the custom diplay of the ozon catalogue item"),
+//       Text("custom display"),
+//       FavouritesFeature.addToFavouritesButton(widget.doc)
+//     ]
+//         // object.fields.map((field) => DataFieldDisplay(field)).toList()
+//         );
+//   }
+// }
--- lib/sample_apps/todoist/todoist.dart
+++ lib/sample_apps/todoist/todoist.dart
@@ -1,125 +1,125 @@
-import 'package:flutter/material.dart';
-import 'package:soar_quest/app/app.dart';
-import 'package:soar_quest/components/bool_field_display.dart';
-import 'package:soar_quest/components/col_create_doc_button.dart';
-import 'package:soar_quest/components/doc_delete_button.dart';
-import 'package:soar_quest/components/doc_field_toggle_buttons.dart';
-import 'package:soar_quest/data/firestore.dart';
-import 'package:soar_quest/data/sq_collection.dart';
-import 'package:soar_quest/data/sq_doc.dart';
-import 'package:soar_quest/screens/collection_screen.dart';
-import 'package:soar_quest/screens/doc_screen.dart';
-import 'package:soar_quest/screens/main_screen.dart';
-import 'package:soar_quest/screens/screen.dart';
-import 'package:soar_quest/users/user_data.dart';
+// import 'package:flutter/material.dart';
+// import 'package:soar_quest/app/app.dart';
+// import 'package:soar_quest/components/bool_field_display.dart';
+// import 'package:soar_quest/components/col_create_doc_button.dart';
+// import 'package:soar_quest/components/doc_delete_button.dart';
+// import 'package:soar_quest/components/doc_field_toggle_buttons.dart';
+// import 'package:soar_quest/data/firestore.dart';
+// import 'package:soar_quest/data/sq_collection.dart';
+// import 'package:soar_quest/data/sq_doc.dart';
+// import 'package:soar_quest/screens/collection_screen.dart';
+// import 'package:soar_quest/screens/doc_screen.dart';
+// import 'package:soar_quest/screens/main_screen.dart';
+// import 'package:soar_quest/screens/screen.dart';
+// import 'package:soar_quest/users/user_data.dart';
 
-void main() async {
-  App todoistApp = App("Todoist",
-      theme: ThemeData(primarySwatch: Colors.red, useMaterial3: true));
+// void main() async {
+//   App todoistApp = App("Todoist",
+//       theme: ThemeData(primarySwatch: Colors.red, useMaterial3: true));
 
-  await todoistApp.init();
+//   await todoistApp.init();
 
-  App.instance.currentUser = UserData(userId: "testuser123");
+//   App.instance.currentUser = UserData(userId: "testuser123");
 
-  final todoCollection = FirestoreCollection(
-      id: "Todos",
-      fields: [
-        SQDocField("Task Name", SQDocFieldType.string),
-        SQDocField("Reminder", SQDocFieldType.string),
-        SQDocField("Done", SQDocFieldType.bool, value: false),
-      ],
-      singleDocName: "Todo",
-      userData: true);
+//   final todoCollection = FirestoreCollection(
+//       id: "Todos",
+//       fields: [
+//         SQDocField<String>("Task Name"),
+//         SQDocField<String>("Reminder"),
+//         SQDocField<bool>("Done", value: false),
+//       ],
+//       singleDocName: "Todo",
+//       userData: true);
 
-  final todosScreen = CollectionScreen(
-    "Todos",
-    todoCollection,
-    docScreenBody: TodoDocBody.new,
-    collectionScreenBody: TodosCollectionBody.new,
-  );
+//   final todosScreen = CollectionScreen(
+//     "Todos",
+//     todoCollection,
+//     docScreenBody: TodoDocBody.new,
+//     collectionScreenBody: TodosCollectionBody.new,
+//   );
 
-  final MainScreen homescreen = MainScreen(
-    [todosScreen, Screen("hello")],
-    initialScreenIndex: 0,
-  );
-  todoistApp.homescreen = homescreen;
+//   final MainScreen homescreen = MainScreen(
+//     [todosScreen, Screen("hello")],
+//     initialScreenIndex: 0,
+//   );
+//   todoistApp.homescreen = homescreen;
 
-  todoistApp.run();
-}
+//   todoistApp.run();
+// }
 
-class TodoDocBody extends DocScreenBody {
-  const TodoDocBody(SQDoc doc, {Key? key}) : super(doc, key: key);
+// class TodoDocBody extends DocScreenBody {
+//   const TodoDocBody(SQDoc doc, {Key? key}) : super(doc, key: key);
 
-  @override
-  State<TodoDocBody> createState() => _TodoDocBodyState();
-}
+//   @override
+//   State<TodoDocBody> createState() => _TodoDocBodyState();
+// }
 
-class _TodoDocBodyState extends State<TodoDocBody> {
-  void refresh() {
-    setState(() {});
-  }
+// class _TodoDocBodyState extends State<TodoDocBody> {
+//   void refresh() {
+//     setState(() {});
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text(
-          "Your task ${widget.doc.getFieldValueByName("Task Name")} is due on ${widget.doc.getFieldValueByName("Reminder")}"),
-      Text("Done: ${widget.doc.getFieldValueByName("Done")}"),
-      DocFieldToggleButtons(
-          widget.doc.getFieldByName("Done"), widget.doc, refresh)
-    ]);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//       Text(
+//           "Your task ${widget.doc.getFieldValueByName("Task Name")} is due on ${widget.doc.getFieldValueByName("Reminder")}"),
+//       Text("Done: ${widget.doc.getFieldValueByName("Done")}"),
+//       DocFieldToggleButtons(
+//           widget.doc.getFieldByName("Done"), widget.doc, refresh)
+//     ]);
+//   }
+// }
 
-class TodosCollectionBody extends CollectionScreenBody {
-  const TodosCollectionBody(SQCollection collection,
-      {required Function refreshScreen, Key? key})
-      : super(collection, refreshScreen: refreshScreen, key: key);
+// class TodosCollectionBody extends CollectionScreenBody {
+//   const TodosCollectionBody(SQCollection collection,
+//       {required Function refreshScreen, Key? key})
+//       : super(collection, refreshScreen: refreshScreen, key: key);
 
-  @override
-  State<TodosCollectionBody> createState() => _TodosCollectionBodyState();
-}
+//   @override
+//   State<TodosCollectionBody> createState() => _TodosCollectionBodyState();
+// }
 
-class _TodosCollectionBodyState extends State<TodosCollectionBody> {
-  void refresh() => setState(() {});
+// class _TodosCollectionBodyState extends State<TodosCollectionBody> {
+//   void refresh() => setState(() {});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemCount: widget.collection.docs.length,
-            itemBuilder: (context, i) {
-              SQDoc doc = widget.collection.docs[i];
-              return ListTile(
-                title: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: BoolFieldDisplay(doc.getFieldByName("Done"), doc,
-                          Icon(Icons.check), Icon(Icons.cancel_outlined)),
-                    ),
-                    Text(
-                      doc.identifier,
-                    ),
-                  ],
-                ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    DocFieldToggleButtons(
-                        doc.getFieldByName("Done"), doc, refresh),
-                    DocDeleteButton(doc, popAfterDelete: false, isIcon: true),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
-        CollectionCreateDocButton(widget.collection)
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Expanded(
+//           child: ListView.builder(
+//             padding: const EdgeInsets.all(16.0),
+//             itemCount: widget.collection.docs.length,
+//             itemBuilder: (context, i) {
+//               SQDoc doc = widget.collection.docs[i];
+//               return ListTile(
+//                 title: Row(
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: BoolFieldDisplay(doc.getFieldByName("Done"), doc,
+//                           Icon(Icons.check), Icon(Icons.cancel_outlined)),
+//                     ),
+//                     Text(
+//                       doc.identifier,
+//                     ),
+//                   ],
+//                 ),
+//                 trailing: Row(
+//                   mainAxisSize: MainAxisSize.min,
+//                   children: [
+//                     DocFieldToggleButtons(
+//                         doc.getFieldByName("Done"), doc, refresh),
+//                     DocDeleteButton(doc, popAfterDelete: false, isIcon: true),
+//                   ],
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//         CollectionCreateDocButton(widget.collection)
+//       ],
+//     );
+//   }
+// }
