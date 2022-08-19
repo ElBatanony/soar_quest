f855602d5e3aa87b045d02112d16f6109711d204
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-19 10:41:38 +0300

    Add ListField component

---
+++ lib/components/list_field.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:soar_quest/data/sq_doc.dart';
+
+import 'doc_field_field.dart';
+
+class ListField extends StatefulWidget {
+  final SQDocListField listField;
+
+  const ListField(this.listField, {Key? key}) : super(key: key);
+
+  @override
+  State<ListField> createState() => _ListFieldState();
+}
+
+class _ListFieldState extends State<ListField> {
+  void deleteListItem(int index) {
+    setState(() {
+      widget.listField.value.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var listItems = widget.listField.value;
+    var listItemsWidgets = [];
+
+    for (int i = 0; i < listItems.length; i++) {
+      listItemsWidgets.add(ListItemField(listItems[i], deleteItem: () {
+        deleteListItem(i);
+      }));
+    }
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("${widget.listField.name} (List of ${listItems.length})"),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  widget.listField.value
+                      .add(SQDocField.nameless(SQDocFieldType.string));
+                });
+              },
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+        ...listItemsWidgets,
+      ],
+    );
+  }
+}
+
+class ListItemField extends StatefulWidget {
+  final SQDocField listItemField;
+  final Function deleteItem;
+
+  const ListItemField(this.listItemField, {required this.deleteItem, Key? key})
+      : super(key: key);
+
+  @override
+  State<ListItemField> createState() => _ListItemFieldState();
+}
+
+class _ListItemFieldState extends State<ListItemField> {
+  @override
+  Widget build(BuildContext context) {
+    var typesWithoutList =
+        SQDocFieldType.values.where((type) => type != SQDocFieldType.list);
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            DropdownButton<SQDocFieldType>(
+                value: widget.listItemField.type,
+                items: typesWithoutList
+                    .map((type) => DropdownMenuItem<SQDocFieldType>(
+                          value: type,
+                          child: Text(type.name),
+                        ))
+                    .toList(),
+                onChanged: ((value) {
+                  setState(() {
+                    if (value != null) {
+                      widget.listItemField.type = value;
+                      widget.listItemField.value =
+                          widget.listItemField.defaultValue;
+                    }
+                  });
+                })),
+            Expanded(child: DocFieldField(widget.listItemField)),
+            IconButton(
+              onPressed: () {
+                widget.deleteItem();
+              },
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
