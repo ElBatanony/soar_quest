75b0778e2d965198d4dd85b55b644f7216a03f9a
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-19 07:47:24 +0300

    Create DocsFilter and StringContainsFilter

---
+++ lib/data/docs_filter.dart
@@ -0,0 +1,25 @@
+import 'package:soar_quest/data.dart';
+
+class DocsFilter {
+  SQDocField field;
+
+  DocsFilter(this.field);
+
+  List<SQDoc> filter(List<SQDoc> docs) {
+    return docs
+        .where((doc) => doc.getFieldValueByName(field.name) == field.value)
+        .toList();
+  }
+}
+
+class StringContainsFilter extends DocsFilter {
+  StringContainsFilter(super.field);
+
+  @override
+  List<SQDoc> filter(List<SQDoc> docs) {
+    return docs
+        .where((doc) => (doc.getFieldValueByName(field.name) as String)
+            .contains(field.value))
+        .toList();
+  }
+}
