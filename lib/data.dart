9f31df33dab970c4fc1b2d7933c92f257723f1c0
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-22 16:10:32 +0300

    Export docs_filter.dart from data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -1,7 +1,10 @@
 import 'app/app.dart';
 import 'data/firestore.dart';
+
 export 'data/sq_collection.dart';
 export 'data/sq_doc.dart';
 export 'data/sq_doc_field.dart';
+export 'data/docs_filter.dart';
+
 FirestoreCollection userCollection = FirestoreCollection(
     id: "users/${App.instance.currentUser.userId}/data", fields: []);
