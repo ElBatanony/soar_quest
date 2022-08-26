f0b362e991ec43b98ff39d2cc9c4dd7b636a7012
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-26 10:01:05 +0300

    Export SQFile from data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -5,6 +5,7 @@ export 'data/sq_collection.dart';
 export 'data/sq_doc.dart';
 export 'data/sq_doc_field.dart';
 export 'data/docs_filter.dart';
+export 'data/sq_file.dart';
 
 FirestoreCollection userCollection = FirestoreCollection(
     id: "users/${App.instance.currentUser.userId}/data", fields: []);
