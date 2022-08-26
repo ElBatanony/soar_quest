7d0e05d07916be03abd9d62f00f5b17753f8ef02
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-26 12:43:17 +0300

    Comment out Webflow Sample App

--- lib/sample_apps/webflow_site/webflow_site.dart
+++ lib/sample_apps/webflow_site/webflow_site.dart
@@ -1,43 +1,43 @@
-import 'package:flutter/material.dart';
-import 'package:soar_quest/app/app.dart';
-import 'package:soar_quest/other_tools/webflow/elements/basic.dart';
-import 'package:soar_quest/other_tools/webflow/elements/layout.dart';
-import 'package:soar_quest/screens/main_screen.dart';
-import 'package:soar_quest/screens/screen.dart';
-import 'package:soar_quest/users/user_data.dart';
+// import 'package:flutter/material.dart';
+// import 'package:soar_quest/app/app.dart';
+// import 'package:soar_quest/other_tools/webflow/elements/basic.dart';
+// import 'package:soar_quest/other_tools/webflow/elements/layout.dart';
+// import 'package:soar_quest/screens/main_screen.dart';
+// import 'package:soar_quest/screens/screen.dart';
+// import 'package:soar_quest/users/user_data.dart';
 
-void main() async {
-  App webflowSiteApp = App("Webflow Site",
-      inDebug: false,
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true));
+// void main() async {
+//   App webflowSiteApp = App("Webflow Site",
+//       inDebug: false,
+//       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true));
 
-  webflowSiteApp.init();
+//   webflowSiteApp.init();
 
-  App.instance.currentUser = UserData(userId: "testuser123");
+//   App.instance.currentUser = UserData(userId: "testuser123");
 
-  webflowSiteApp.homescreen = MainScreen(
-    [WebflowTestScreen("Webflow Test Screen")],
-    initialScreenIndex: 0,
-  );
+//   webflowSiteApp.homescreen = MainScreen(
+//     [WebflowTestScreen("Webflow Test Screen")],
+//     initialScreenIndex: 0,
+//   );
 
-  webflowSiteApp.run();
-}
+//   webflowSiteApp.run();
+// }
 
-class WebflowTestScreen extends Screen {
-  const WebflowTestScreen(String title, {Key? key}) : super(title, key: key);
+// class WebflowTestScreen extends Screen {
+//   const WebflowTestScreen(String title, {Key? key}) : super(title, key: key);
 
-  @override
-  State<WebflowTestScreen> createState() => _WebflowTestScreenState();
-}
+//   @override
+//   State<WebflowTestScreen> createState() => _WebflowTestScreenState();
+// }
 
-class _WebflowTestScreenState extends State<WebflowTestScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return WebflowSection(
-      children: [
-        WebflowContainer(
-            children: [WebflowButton(buttonText: "Webflow Button")])
-      ],
-    );
-  }
-}
+// class _WebflowTestScreenState extends State<WebflowTestScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return WebflowSection(
+//       children: [
+//         WebflowContainer(
+//             children: [WebflowButton(buttonText: "Webflow Button")])
+//       ],
+//     );
+//   }
+// }
