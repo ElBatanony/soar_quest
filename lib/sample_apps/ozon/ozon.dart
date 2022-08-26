2c1ef14859b1acc348555c88eae37dc6c5246eaa
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-26 11:55:43 +0300

    Update Ozon sample app

--- lib/sample_apps/ozon/ozon.dart
+++ lib/sample_apps/ozon/ozon.dart
@@ -1,39 +1,35 @@
-// // ignore_for_file: unused_local_variable, unused_import
-
-// import 'package:firebase_core/firebase_core.dart';
 // import 'package:flutter/material.dart';
 // import 'package:soar_quest/app/app.dart';
-// import 'package:soar_quest/app/app_display.dart';
+// import 'package:soar_quest/app/app_navigator.dart';
+// import 'package:soar_quest/components/buttons/sq_button.dart';
 // import 'package:soar_quest/data/firestore.dart';
-// import 'package:soar_quest/data/sq_collection.dart';
-// import 'package:soar_quest/data/sq_doc.dart';
+// import 'package:soar_quest/data.dart';
 // import 'package:soar_quest/features/favourites/favourites.dart';
-// import 'package:soar_quest/firebase_options.dart';
 // import 'package:soar_quest/screens/collection_screen.dart';
 // import 'package:soar_quest/screens/doc_screen.dart';
 // import 'package:soar_quest/screens/main_screen.dart';
-// import 'package:soar_quest/screens/menu_screen.dart';
 // import 'package:soar_quest/screens/screen.dart';
 // import 'package:soar_quest/users/user_data.dart';
 
 // void main() async {
 //   App ozonApp = App("Ozon");
+//   await ozonApp.init();
+
 //   App.instance.currentUser = UserData(userId: "testuser123");
 
 //   final catalogueCollection = FirestoreCollection(id: "catalogue", fields: [
-//     SQDocField<String>("Item Name"),
+//     SQStringField("Item Name"),
 //     SQDocField<int>("Item Price"),
 //   ]);
 
-//   final catalogueScreen = CollectionScreen(
+//   final catalogueScreen = CatalogueScreen(
 //     "Catalogue",
-//     catalogueCollection,
-//     docScreenBody: CatalogueDocBody.new,
+//     collection: catalogueCollection,
 //   );
 
 //   FavouritesFeature.loadFavourites();
 
-//   final MainScreen homescreen = MainScreen(
+//   ozonApp.homescreen = MainScreen(
 //     [
 //       Screen("Main"),
 //       Screen('Fresh'),
@@ -43,27 +39,51 @@
 //     ],
 //     initialScreenIndex: 2,
 //   );
-//   ozonApp.homescreen = homescreen;
 
 //   ozonApp.run();
 // }
 
-// class CatalogueDocBody extends DocScreenBody {
-//   const CatalogueDocBody(SQDoc doc, {Key? key}) : super(doc, key: key);
+// class CatalogueDocBody extends DocScreen {
+//   CatalogueDocBody(super.title, super.doc,
+//       {required super.refreshCollectionScreen, super.key});
 
 //   @override
 //   State<CatalogueDocBody> createState() => _CataglogueDocBodyState();
 // }
 
-// class _CataglogueDocBodyState extends State<CatalogueDocBody> {
+// class _CataglogueDocBodyState extends DocScreenState<CatalogueDocBody> {
 //   @override
 //   Widget build(BuildContext context) {
-//     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-//       Text("this is the custom diplay of the ozon catalogue item"),
-//       Text("custom display"),
-//       FavouritesFeature.addToFavouritesButton(widget.doc)
-//     ]
-//         // object.fields.map((field) => DataFieldDisplay(field)).toList()
-//         );
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.doc.identifier),
+//       ),
+//       body: Center(
+//         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//           Text("this is the custom diplay of the ozon catalogue item"),
+//           Text("custom display"),
+//           FavouritesFeature.addToFavouritesButton(widget.doc)
+//         ]),
+//       ),
+//     );
+//   }
+// }
+
+// class CatalogueScreen extends CollectionScreen {
+//   CatalogueScreen(super.title, {required super.collection, super.key});
+
+//   @override
+//   State<CatalogueScreen> createState() => _CatalogueScreenState();
+// }
+
+// class _CatalogueScreenState extends CollectionScreenState<CatalogueScreen> {
+//   @override
+//   Widget docDisplay(SQDoc doc) {
+//     return SQButton(doc.identifier, onPressed: () {
+//       goToScreen(
+//           CatalogueDocBody("doc page", doc,
+//               refreshCollectionScreen: refreshScreen),
+//           context: context);
+//     });
 //   }
 // }
