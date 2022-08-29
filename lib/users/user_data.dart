c797b0a4cf8556d5b479cab2680b517727c60b5b
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-29 09:49:28 +0300

    Add isAnonymous field to UserData

--- lib/users/user_data.dart
+++ lib/users/user_data.dart
@@ -2,8 +2,9 @@ import 'package:soar_quest/app/app.dart';
 
 class UserData {
   String userId;
+  bool isAnonymous;
 
-  UserData({required this.userId});
+  UserData({required this.userId, required this.isAnonymous});
 
   userDataPath() {
     return "${App.instance.getAppPath()}users/$userId/data/";
