e4296fed045d09e905befbae6ea0d0e252474fc5
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-29 14:03:33 +0300

    Add update user fields functions to ProfileScreen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:soar_quest/components/buttons/sq_button.dart';
 
 import '../app/app.dart';
+import '../components/doc_field_field.dart';
 import '../components/signed_in_content.dart';
 import '../data.dart';
 import 'doc_screen.dart';
@@ -19,23 +20,72 @@ class ProfileScreen extends DocScreen {
 }
 
 class _ProfileScreenState extends DocScreenState<ProfileScreen> {
+  signOut() async {
+    await App.auth.signOut();
+    refresh();
+  }
+
+  updateUserField(SQDocField field, Function updateFunction) async {
+    dynamic newValue = await showFieldDialog(context: context, field: field);
+    if (newValue != null) {
+      await updateFunction(newValue);
+      refresh();
+    }
+  }
+
+  updateUsername(SignedInUser user) async {
+    return updateUserField(
+        SQStringField("Username", value: user.displayName ?? ""),
+        user.updateDisplayName);
+  }
+
+  updateEmail(SignedInUser user) async {
+    try {
+      await updateUserField(
+          SQStringField("Email", value: user.email ?? ""), user.updateEmail);
+    } catch (e) {
+      print(e.toString());
+      await App.auth.goToSignIn(context, forceSignIn: true);
+    }
+  }
+
+  updatePassword(SignedInUser user) async {
+    try {
+      await updateUserField(SQStringField("Password"), user.updatePassword);
+    } catch (e) {
+      print(e.toString());
+      await App.auth.goToSignIn(context, forceSignIn: true);
+    }
+  }
+
   @override
   Widget screenBody(BuildContext context) {
-    return Column(
-      children: [
-        Text("User ID: ${App.auth.user.userId}"),
-        Text("User Anonymous: ${App.auth.user.isAnonymous}"),
-        SignedInContent(child: Text("Important stuff for non-anonymous users")),
-        SignedInContent(
-            child: SQButton("Sign out", onPressed: () async {
-              await App.auth.signOut();
-              setState(() {});
-            }),
-            refreshUp: () {
-              setState(() {});
-            }),
-        super.screenBody(context),
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Text("User ID: ${App.auth.user.userId}"),
+          Text("User Anonymous: ${App.auth.user.isAnonymous}"),
+          SignedInContent(
+              builder: (BuildContext context, SignedInUser user) {
+                return Column(
+                  children: [
+                    Text("Important stuff for non-anonymous users"),
+                    Text("Username: ${user.displayName}"),
+                    Text("Email: ${user.email ?? "No-email"}"),
+                    // super.screenBody(context),
+                    SQButton('Update username',
+                        onPressed: () => updateUsername(user)),
+                    SQButton('Update email',
+                        onPressed: () => updateEmail(user)),
+                    SQButton('Update password',
+                        onPressed: () => updatePassword(user)),
+                    SQButton("Sign out", onPressed: signOut),
+                  ],
+                );
+              },
+              refreshUp: refresh),
+        ],
+      ),
     );
   }
 }
