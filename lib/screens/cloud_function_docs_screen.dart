ac3967e19598783623a3ba95c96a45ace6274440
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-29 14:32:21 +0300

    Use docsDisplay in CloudFunctionDocsScreen

--- lib/screens/cloud_function_docs_screen.dart
+++ lib/screens/cloud_function_docs_screen.dart
@@ -3,21 +3,17 @@ import 'package:flutter/material.dart';
 import '../cloud_functions/get_collections_docs.dart';
 import '../data.dart';
 import 'collection_screen.dart';
-import 'screen.dart';
 
-class CloudFunctionDocsScreen extends Screen {
-  final SQCollection collection;
-
-  const CloudFunctionDocsScreen(String title,
-      {required this.collection, Key? key})
-      : super(title, key: key);
+class CloudFunctionDocsScreen extends CollectionScreen {
+  CloudFunctionDocsScreen(super.title, {required super.collection, super.key});
 
   @override
   State<CloudFunctionDocsScreen> createState() =>
       _CloudFunctionDocsScreenState();
 }
 
-class _CloudFunctionDocsScreenState extends State<CloudFunctionDocsScreen> {
+class _CloudFunctionDocsScreenState
+    extends CollectionScreenState<CloudFunctionDocsScreen> {
   List<SQDoc> fetchedDocs = [];
 
   fetchDocsFromCloudFunction() async {
@@ -33,6 +29,11 @@ class _CloudFunctionDocsScreenState extends State<CloudFunctionDocsScreen> {
   }
 
   @override
+  List<Widget> docsDisplay(BuildContext context) {
+    return fetchedDocs.map((doc) => docDisplay(doc)).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
@@ -40,10 +41,7 @@ class _CloudFunctionDocsScreenState extends State<CloudFunctionDocsScreen> {
         child: Column(
           children: [
             Text('${widget.title} Screen'),
-            ...fetchedDocs
-                .map((doc) =>
-                    CollectionScreenDocButton(doc, widget.collection, () {}))
-                .toList()
+            ...docsDisplay(context),
           ],
         ),
       ),
