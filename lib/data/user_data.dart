a045fbbe527189647e381ddd8e7fd657a07b94e8
ElBatanony <ahmedelbatanony@hotmail.com>
2022-08-29 13:56:10 +0300

    Create SignedInUser class

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
 import '../app/app.dart';
 import '../data.dart';
 
@@ -13,9 +15,60 @@ class UserData {
 }
 
 List<SQDocField> userDocFields = [
-  SQStringField("Nickname"),
+  SQStringField("City"),
   SQTimestampField("Birthdate")
 ];
 
 late SQCollection userCollection;
 late SQDoc userDoc;
+
+abstract class SignedInUser extends UserData {
+  SignedInUser({required super.userId, required super.isAnonymous});
+
+  String? get email;
+  String? get displayName;
+
+  Future updateEmail(String newEmail);
+
+  Future updatePassword(String newPassword);
+
+  Future<void> updateDisplayName(String displayName);
+}
+
+class FirebaseSignedInUser extends SignedInUser {
+  firebase_auth.User firebaseUser;
+
+  FirebaseSignedInUser(this.firebaseUser)
+      : super(
+          userId: firebaseUser.uid,
+          isAnonymous: firebaseUser.isAnonymous,
+        );
+
+  refreshUser() {
+    firebaseUser = firebase_auth.FirebaseAuth.instance.currentUser!;
+  }
+
+  @override
+  String? get email => firebaseUser.email;
+
+  @override
+  String? get displayName => firebaseUser.displayName;
+
+  @override
+  Future updateEmail(String newEmail) async {
+    await firebaseUser.updateEmail(newEmail);
+    refreshUser();
+  }
+
+  @override
+  Future updatePassword(String newPassword) async {
+    await firebaseUser.updatePassword(newPassword);
+    refreshUser();
+  }
+
+  @override
+  Future<void> updateDisplayName(String displayName) async {
+    await firebaseUser.updateDisplayName(displayName);
+    refreshUser();
+  }
+}
