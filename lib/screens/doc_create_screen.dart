66a1bf01b3d9e9358964d31b0d98168e290d96ba
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-14 19:50:32 +0300

    Refactor DocCreateScreen

--- lib/screens/doc_create_screen.dart
+++ lib/screens/doc_create_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:soar_quest/app/app_navigator.dart';
-import 'package:soar_quest/components/doc_field_field.dart';
-import 'package:soar_quest/data/sq_collection.dart';
-import 'package:soar_quest/data/sq_doc.dart';
-import 'package:soar_quest/screens/screen.dart';
+
+import '../../data.dart';
+import '../app/app_navigator.dart';
+import '../components/buttons/sq_button.dart';
+import '../components/doc_field_field.dart';
+import 'screen.dart';
 
 class DocCreateScreen extends Screen {
   final SQCollection collection;
@@ -11,12 +12,14 @@ class DocCreateScreen extends Screen {
   final List<SQDocField> initialFields;
   final List<String> hiddenFields;
 
-  const DocCreateScreen(String title, this.collection,
-      {this.createCallback,
-      this.initialFields = const [],
-      this.hiddenFields = const [],
-      Key? key})
-      : super(title, key: key);
+  const DocCreateScreen(
+    super.title,
+    this.collection, {
+    this.createCallback,
+    this.initialFields = const [],
+    this.hiddenFields = const [],
+    super.key,
+  });
 
   @override
   State<DocCreateScreen> createState() => _DocCreateScreenState();
@@ -25,26 +28,19 @@ class DocCreateScreen extends Screen {
 class _DocCreateScreenState extends ScreenState<DocCreateScreen> {
   late SQDoc newDoc;
 
-  void loadData() async {
-    await widget.collection.loadCollection();
-    setState(() {});
-  }
-
   @override
   void initState() {
-    loadData();
     String newDocId = widget.collection.getANewDocId();
     newDoc = SQDoc(newDocId, collection: widget.collection);
 
-    for (var field in widget.initialFields) {
+    for (var field in widget.initialFields)
       newDoc.getFieldByName(field.name).value = field.value;
-    }
 
     super.initState();
   }
 
-  void saveItem() async {
-    newDoc.collection.createDoc(newDoc).then((value) {
+  void createDoc() async {
+    widget.collection.createDoc(newDoc).then((value) {
       if (widget.createCallback != null) widget.createCallback!();
       exitScreen(context);
     });
@@ -55,29 +51,10 @@ class _DocCreateScreenState extends ScreenState<DocCreateScreen> {
     return SingleChildScrollView(
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '${widget.title} Screen',
-            ),
-            Text('Doc path: ${widget.collection.getPath()}'),
-            Center(
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 350),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [Text("Doc ID (generated)"), Text(newDoc.id)],
-                      ),
-                      ...DocFieldField.generateDocFieldsFields(newDoc,
-                          hiddenFields: widget.hiddenFields),
-                      ElevatedButton(
-                          onPressed: saveItem, child: Text("Save / Insert"))
-                    ]),
-              ),
-            )
+            ...DocFieldField.generateDocFieldsFields(newDoc,
+                hiddenFields: widget.hiddenFields),
+            SQButton("Create", onPressed: createDoc)
           ],
         ),
       ),
