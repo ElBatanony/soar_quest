6aa705ef55559d863f542bc1ee7944c568163733
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-14 20:09:22 +0300

    Create SpecialAccessContent widget prototype

---
+++ lib/components/special_access_content.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../app/app.dart';
+import '../data.dart';
+
+class SpecialAccessContent extends StatefulWidget {
+  final Widget child;
+  final bool Function(UserData user) checker;
+
+  const SpecialAccessContent(
+      {required this.child, required this.checker, super.key});
+
+  @override
+  State<SpecialAccessContent> createState() => _SpecialAccessContentState();
+}
+
+class _SpecialAccessContentState extends State<SpecialAccessContent> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.checker(App.auth.user)
+        ? Text("You do not have access to this content")
+        : widget.child;
+  }
+}
