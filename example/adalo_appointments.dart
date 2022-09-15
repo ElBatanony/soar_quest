1e347534cdd9c7822128357518da1bafadb20cd4
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-15 20:06:12 +0300

    Add Settings and Fav to Adalo Appointment

--- example/adalo_appointments.dart
+++ example/adalo_appointments.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:soar_quest/app.dart';
 import 'package:soar_quest/data.dart';
+import 'package:soar_quest/data/fields/sq_time_of_day_field.dart';
 import 'package:soar_quest/data/fields/sq_user_ref_field.dart';
 import 'package:soar_quest/features/favourites/favourites.dart';
 import 'package:soar_quest/screens/collection_screen.dart';
@@ -15,8 +16,12 @@ void main() async {
     SQFileField("Profile Picture"),
   ];
 
+  AppSettings settings = AppSettings(
+      settingsFields: [SQBoolField("test field"), SQStringField("waow")]);
+
   App adaloAppointmentsApp = App("First Class",
       theme: ThemeData(primaryColor: Colors.green, useMaterial3: true),
+      settings: settings,
       userDocFields: userDocFields);
 
   await adaloAppointmentsApp.init();
@@ -34,11 +39,14 @@ void main() async {
   SQCollection requests = FirestoreCollection(id: "Requests", fields: [
     SQDocReferenceField("Requested Class", collection: classes),
     SQTimestampField("Requested Class Date"),
+    SQTimeOfDayField("Time"),
     SQStringField("Status"),
     SQEditedByField("Attendee"),
     SQStringField("Video Meeting Link"),
     SQStringField("Reschedule Comment")
   ]);
+
+  FavouritesFeature favouriteClasses = FavouritesFeature(collection: classes);
 
   adaloAppointmentsApp.homescreen = MainScreen([
     CollectionScreen(
@@ -49,14 +57,13 @@ void main() async {
       collection: classes,
       docScreen: (doc) => DocScreen(
         doc,
-        postbody: FavouritesFeature.addToFavouritesButton(doc),
+        postbody: favouriteClasses.addToFavouritesButton(doc),
       ),
     ),
     // CollectionScreen("Class Types", collection: classTypes),
-    ProfileScreen(
-      "Profile",
-      postbody: FavouritesScreenButton(),
-    ),
+    favouriteClasses.favouritesScreen,
+    ProfileScreen("Profile"),
+    settingsScreen()
   ]);
 
   adaloAppointmentsApp.run();
