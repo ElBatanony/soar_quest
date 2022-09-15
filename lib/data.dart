ae551be8ecc5de84a7670ae86adf9b30c95490da
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-15 09:02:50 +0300

    Move SQFile to data/types

--- lib/data.dart
+++ lib/data.dart
@@ -2,6 +2,6 @@ export 'data/sq_collection.dart';
 export 'data/sq_doc.dart';
 export 'data/sq_doc_field.dart';
 export 'data/docs_filter.dart';
-export 'data/sq_file.dart';
+export 'data/types/sq_file.dart';
 export 'data/user_data.dart';
 export 'data/firestore.dart';
