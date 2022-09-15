7c0a8e51df6f3f8807afbd1e350cfba90415cb0c
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-15 09:04:03 +0300

    Move SQDocReferenceField to data/fields

--- lib/data/fields/sq_doc_reference_field.dart
+++ lib/data/fields/sq_doc_reference_field.dart
@@ -1,5 +1,5 @@
-import '../data.dart';
-import 'types/sq_doc_reference.dart';
+import '../../data.dart';
+import '../types/sq_doc_reference.dart';
 
 class SQDocReferenceField extends SQDocField<SQDocReference> {
   SQCollection collection;
