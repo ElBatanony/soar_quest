e2456aa618a4dedfb7f04b97006719dc118d772c
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-18 21:20:18 +0300

    Add LearnScreen to Adalo Appointments

--- example/adalo/adalo_appointments.dart
+++ example/adalo/adalo_appointments.dart
@@ -43,7 +43,7 @@ void main() async {
       settingsFields: [SQBoolField("test field"), SQStringField("waow")]);
 
   App adaloAppointmentsApp = App("First Class",
-      theme: ThemeData(primaryColor: Colors.green, useMaterial3: true),
+      theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
       settings: settings,
       userDocFields: userDocFields);
 
@@ -137,8 +137,7 @@ void main() async {
           CollectionScreen(title: "All Classes", collection: classes)
               .button(context),
     ),
-    CollectionScreen(title: "Learn", collection: requests),
-
+    LearnScreen(collection: requests),
     CollectionScreen(collection: classTypes, docScreen: classTypeDocScreen),
     // CollectionScreen("Class Types", collection: classTypes),
     FavouritesScreen(
@@ -157,4 +156,86 @@ void main() async {
   ]);
 
   adaloAppointmentsApp.run();
+}
+
+class LearnScreen extends CollectionScreen {
+  LearnScreen({required super.collection, super.key}) : super(title: "Learn");
+
+  @override
+  State<LearnScreen> createState() => _LearnScreenState();
+}
+
+class _LearnScreenState extends CollectionScreenState<LearnScreen> {
+  @override
+  Widget docDisplay(SQDoc doc) {
+    return LearnDocWidget(doc);
+  }
+
+  @override
+  Widget screenBody(BuildContext context) {
+    return Column(
+      children: [
+        Text("My Upcoming Classes"),
+        super.screenBody(context),
+      ],
+    );
+  }
+}
+
+class LearnDocWidget extends StatefulWidget {
+  final SQDoc doc;
+
+  const LearnDocWidget(this.doc, {super.key});
+
+  @override
+  State<LearnDocWidget> createState() => _LearnDocWidgetState();
+}
+
+class _LearnDocWidgetState extends State<LearnDocWidget> {
+  SQDoc? requestClassDoc;
+  SQDocReference? teacherDocRef;
+
+  void loadData() async {
+    requestClassDoc =
+        (widget.doc.getFieldValueByName("Requested Class") as SQDocReference)
+            .getDoc();
+    await requestClassDoc!.loadDoc();
+    teacherDocRef = requestClassDoc!.getFieldValueByName("Teacher");
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    loadData();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SQDocReference classRef = widget.doc.getFieldValueByName("Requested Class");
+    print(classRef.docId);
+    return GestureDetector(
+      onTap: () => goToScreen(DocScreen(widget.doc), context: context),
+      child: Card(
+        color: Colors.lightGreen[100],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                widget.doc.identifier,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text("Status: ${widget.doc.getFieldValueByName("Status")}"),
+              if (requestClassDoc?.initialized == true)
+                Text("Teacher: ${teacherDocRef!.docIdentifier}"),
+              Text(
+                  "Requested Date: ${widget.doc.getFieldValueByName("Requested Class Date")}"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
