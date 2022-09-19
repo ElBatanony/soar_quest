c497fc0e4d2cfcf0ed0c0987f5fdfc18a1476928
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-19 19:48:08 +0300

    Transform DocCreateScreen into a method

--- lib/screens/doc_create_screen.dart
+++ lib/screens/doc_create_screen.dart
@@ -1,73 +1,39 @@
-import 'package:flutter/material.dart';
-
-import '../app.dart';
-import '../components/buttons/sq_button.dart';
-import '../components/doc_form_field.dart';
 import '../data/db.dart';
-import 'screen.dart';
+import 'doc_form_screen.dart';
 
-class DocCreateScreen extends Screen {
-  final SQCollection collection;
-  final List<SQDocField> initialFields;
-  final List<String> hiddenFields;
-  final String submitButtonText;
-
-  DocCreateScreen(
-    this.collection, {
-    String? title,
-    this.initialFields = const [],
-    this.hiddenFields = const [],
-    this.submitButtonText = "Create",
-    super.key,
-  }) : super(title ?? "Create ${collection.singleDocName}");
-
-  @override
-  State<DocCreateScreen> createState() => _DocCreateScreenState();
-
-  @override
-  SQButton button(BuildContext context, {String? label}) {
-    return super.button(context,
-        label: label ?? "Create a ${collection.singleDocName}");
-  }
+Future createDoc(SQDoc doc) async {
+  return doc.collection.createDoc(doc);
 }
 
-class _DocCreateScreenState extends ScreenState<DocCreateScreen> {
-  late SQDoc newDoc;
+DocFormScreen docCreateScreen(
+  SQCollection collection, {
+  String? title,
+  List<SQDocField> initialFields = const [],
+  List<String> hiddenFields = const [],
+  String submitButtonText = "Create",
+}) {
+  title ??= "Create ${collection.singleDocName}";
 
-  @override
-  void initState() {
-    String newDocId = widget.collection.getANewDocId();
-    newDoc = SQDoc(newDocId, collection: widget.collection);
+  String newDocId = collection.getANewDocId();
+  SQDoc newDoc = SQDoc(newDocId, collection: collection);
 
-    for (var field in widget.initialFields) {
-      SQDocField? newDocField = newDoc.getFieldByName(field.name);
-      newDocField?.value = field.value;
-      newDocField?.readOnly = field.readOnly;
-    }
-
-    for (var field in newDoc.fields)
-      if (field.runtimeType == SQEditedByField)
-        field.value = SQUserRefField.currentUserRef;
-
-    super.initState();
+  for (var field in initialFields) {
+    SQDocField? newDocField = newDoc.getFieldByName(field.name);
+    newDocField?.value = field.value;
+    newDocField?.readOnly = field.readOnly;
   }
 
-  void createDoc() async {
-    await widget.collection.createDoc(newDoc).then((_) => exitScreen(context));
-  }
+  return DocFormScreen(
+    newDoc,
+    submitFunction: createDoc,
+    title: title,
+    hiddenFields: hiddenFields,
+    submitButtonText: submitButtonText,
+  );
 
-  @override
-  Widget screenBody(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            ...DocFormField.generateDocFieldsFields(newDoc,
-                hiddenFields: widget.hiddenFields),
-            SQButton(widget.submitButtonText, onPressed: createDoc)
-          ],
-        ),
-      ),
-    );
-  }
+  // @override
+  // SQButton button(BuildContext context, {String? label}) {
+  //   return super.button(context,
+  //       label: label ?? "Create a ${collection.singleDocName}");
+  // }
 }
