07e035caa8b4b59c1ed95c764f59d900f00f04a3
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-19 19:46:08 +0300

    Create DocFormScreen

--- lib/screens/doc_form_screen.dart
+++ lib/screens/doc_form_screen.dart
@@ -1,3 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../app.dart';
+import '../components/buttons/sq_button.dart';
+import '../components/doc_form_field.dart';
+import '../data/db.dart';
+import 'screen.dart';
+
+class DocFormScreen extends Screen {
+  late final SQCollection collection;
+  final List<String> hiddenFields;
+  final String submitButtonText;
+  final List<String> shownFields;
+  final SQDoc doc;
+  final Future Function(SQDoc) submitFunction;
+
+  DocFormScreen(
+    this.doc, {
+    String? title,
+    required this.submitFunction,
+    this.hiddenFields = const [],
+    this.shownFields = const [],
+    this.submitButtonText = "Submit",
+    super.key,
+  }) : super(title ?? "Edit ${doc.collection.singleDocName}") {
+    collection = doc.collection;
+  }
+
+  @override
+  State<DocFormScreen> createState() => DocFormScreenState();
+
+  @override
+  SQButton button(BuildContext context, {String? label}) {
+    return super
+        .button(context, label: label ?? "Edit a ${collection.singleDocName}");
+  }
+}
+
+class DocFormScreenState<T extends DocFormScreen> extends ScreenState<T> {
+  @override
+  void initState() {
+    for (var field in widget.doc.fields)
+      if (field.runtimeType == SQEditedByField)
+        field.value = SQUserRefField.currentUserRef;
+
+    super.initState();
+  }
+
+  Future submitForm() async {
+    await widget.submitFunction(widget.doc).then(
+          (_) => exitScreen(context, value: true),
+        );
+  }
+
+  @override
+  Widget screenBody(BuildContext context) {
+    return Column(
+      children: [
+        ...generateDocFieldsFields(widget.doc,
+            hiddenFields: widget.hiddenFields),
+        SQButton(widget.submitButtonText, onPressed: submitForm)
+      ],
+    );
+  }
+}
+
 List<DocFormField> generateDocFieldsFields(
   SQDoc doc, {
   List<String> hiddenFields = const [],
