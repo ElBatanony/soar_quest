29e8f36e903992d5941e98076864665cc10d8574
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-19 19:13:26 +0300

    Add shownFields to generateDocFieldsFields

--- lib/components/doc_form_field.dart
+++ lib/components/doc_form_field.dart
@@ -27,9 +27,20 @@ class DocFormField extends StatefulWidget {
   static List<DocFormField> generateDocFieldsFields(
     SQDoc doc, {
     List<String> hiddenFields = const [],
+    List<String>? shownFields,
   }) {
-    return doc.fields
+    List<SQDocField> fields = doc.fields;
+
+    if (shownFields != null)
+      fields = fields
+          .where((field) => shownFields.contains(field.name) == true)
+          .toList();
+
+    fields = fields
         .where((field) => hiddenFields.contains(field.name) == false)
+        .toList();
+
+    return fields
         .map((field) => DocFormField(
               field,
               doc: doc,
