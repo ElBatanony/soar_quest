753e841c124bbaa5adab13023f295d4e68c40a62
ElBatanony <ahmedelbatanony@hotmail.com>
2022-09-20 11:50:47 +0300

    Add VideoLinkField.formField as SQStringFormField

--- lib/data/fields/sq_video_link_field.dart
+++ lib/data/fields/sq_video_link_field.dart
@@ -12,4 +12,9 @@ class VideoLinkField extends SQDocField<String> {
 
   @override
   VideoLinkField copy() => VideoLinkField(name, url: value, readOnly: readOnly);
+
+  @override
+  DocFormField formField({Function? onChanged, SQDoc? doc}) {
+    return SQStringFormField(this, onChanged: onChanged);
+  }
 }
