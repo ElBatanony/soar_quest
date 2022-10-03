d8d55a19629cde631f6fb2392242ddf242a63264
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-03 10:58:11 +0300

    Move SQFieldListFormField to SQFieldListField

--- lib/data/fields/sq_field_list_field.dart
+++ lib/data/fields/sq_field_list_field.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../../app.dart';
+import '../../components/buttons/sq_button.dart';
 import '../fields.dart';
 
 class SQFieldListField extends SQListField<SQDocField> {
@@ -43,6 +47,93 @@ class SQFieldListField extends SQListField<SQDocField> {
 
   @override
   DocFormField formField({Function? onChanged, SQDoc? doc}) {
-    return SQFieldListFormField(this, onChanged: onChanged);
+    return _SQFieldListFormField(this, onChanged: onChanged);
+  }
+}
+
+Future showFieldOptions(SQFieldListField fieldListfield,
+    {required BuildContext context}) {
+  return showDialog<SQDocField>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            title: Text("Select new field type"),
+            content: Wrap(
+              children: [
+                ...fieldListfield.allowedTypes
+                    .map((field) => SQButton(field.type.toString(),
+                        onPressed: () =>
+                            exitScreen(context, value: field.copy())))
+                    .toList(),
+              ],
+            ),
+            actions: [
+              SQButton('Cancel', onPressed: () => exitScreen(context)),
+            ]);
+      });
+}
+
+class _SQFieldListFormField extends DocFormField<SQFieldListField> {
+  final SQFieldListField listField;
+
+  const _SQFieldListFormField(this.listField, {required super.onChanged})
+      : super(listField);
+
+  @override
+  createState() => _SQFieldListFormFieldState();
+}
+
+class _SQFieldListFormFieldState extends DocFormFieldState<SQFieldListField> {
+  SQFieldListField get listField => field;
+
+  void deleteListItem(int index) {
+    setState(() {
+      listField.fields.removeAt(index);
+    });
+  }
+
+  void addField() async {
+    SQDocField? newValue = await showFieldOptions(listField, context: context);
+    if (newValue != null) {
+      setState(() {
+        listField.fields.add(newValue);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var listItems = listField.fields;
+    var listItemsWidgets = [];
+
+    for (int i = 0; i < listItems.length; i++) {
+      listItemsWidgets.add(Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: listItems[i].formField()),
+              IconButton(
+                  onPressed: () {
+                    deleteListItem(i);
+                  },
+                  icon: Icon(Icons.delete)),
+            ],
+          ),
+        ],
+      ));
+    }
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("${listField.name} (List of ${listItems.length})"),
+            IconButton(onPressed: addField, icon: Icon(Icons.add)),
+          ],
+        ),
+        ...listItemsWidgets,
+      ],
+    );
   }
 }
