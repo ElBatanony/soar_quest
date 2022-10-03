c36a9e8550085ed7c4e425133b4d8f88ab2531ed
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-03 10:34:06 +0300

    Rename sq_updated_date_field.dart

--- lib/data/fields.dart
+++ lib/data/fields.dart
@@ -6,7 +6,7 @@ export 'fields/sq_file_field.dart';
 export 'fields/sq_time_of_day_field.dart';
 export 'fields/sq_timestamp_field.dart';
 export 'fields/sq_user_ref_field.dart';
-export 'fields/updated_date_field.dart';
+export 'fields/sq_updated_date_field.dart';
 export 'fields/sq_string_field.dart';
 export 'fields/sq_bool_field.dart';
 export 'fields/sq_int_field.dart';
