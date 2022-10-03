26d71ad3e11eb2a6f56785c93f99214d857c768a
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-03 10:33:23 +0300

    Move SQIntFormField to SQIntField

--- lib/data/fields/sq_int_field.dart
+++ lib/data/fields/sq_int_field.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import '../fields.dart';
 
 class SQIntField extends SQDocField<int> {
@@ -14,6 +16,38 @@ class SQIntField extends SQDocField<int> {
 
   @override
   DocFormField formField({Function? onChanged, SQDoc? doc}) {
-    return SQIntFormField(this, onChanged: onChanged);
+    return _SQIntFormField(this, onChanged: onChanged);
+  }
+}
+
+class _SQIntFormField extends DocFormField<SQIntField> {
+  const _SQIntFormField(super.field, {required super.onChanged});
+
+  @override
+  createState() => _SQIntFormFieldState();
+}
+
+class _SQIntFormFieldState extends DocFormFieldState<SQIntField> {
+  final fieldTextController = TextEditingController();
+
+  @override
+  void initState() {
+    fieldTextController.text = (field.value ?? "").toString();
+    super.initState();
+  }
+
+  @override
+  Widget fieldBuilder(BuildContext context) {
+    return TextField(
+      controller: fieldTextController,
+      onChanged: (text) {
+        field.value = int.tryParse(text);
+      },
+      onEditingComplete: onChanged,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: field.name,
+      ),
+    );
   }
 }
