529b7909d46b1036f17f5326b600f9fdfaae584e
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-04 11:04:03 +0300

    Move SQDocField class to fields/sq_doc_field.dart

---
+++ lib/src/fields/sq_doc_field.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../db/sq_doc.dart';
+
+abstract class SQDocField<T> {
+  String name = "";
+  T? value;
+  Type get type => T;
+  bool readOnly;
+  bool required;
+
+  SQDocField(
+    this.name, {
+    this.value,
+    this.readOnly = false,
+    this.required = false,
+  });
+
+  SQDocField copy();
+
+  dynamic collectField() => value;
+
+  T? parse(dynamic source);
+
+  DocFormField formField({Function? onChanged, SQDoc? doc});
+
+  DocFormField readOnlyField({SQDoc? doc}) {
+    return SQReadOnlyFormField(this, doc: doc);
+  }
+
+  @override
+  String toString() {
+    return "${name == "" ? "" : "$name:"} $value";
+  }
+
+  bool get isNull => value == null;
+}
+
+abstract class DocFormField<DocField extends SQDocField>
+    extends FormField<DocFormField<DocField>> {
+  final DocField field;
+  final Function? onChanged;
+  final SQDoc? doc;
+
+  const DocFormField(this.field, {this.onChanged, this.doc, super.key})
+      : super(builder: emptyBuilder);
+
+  static Widget emptyBuilder(FormFieldState s) => Container();
+
+  @override
+  DocFormFieldState<DocField> createState();
+}
+
+abstract class DocFormFieldState<DocField extends SQDocField>
+    extends FormFieldState<DocFormField<DocField>> {
+  DocFormField<DocField> get formField => (widget as DocFormField<DocField>);
+  DocField get field => formField.field;
+  SQDoc? get doc => formField.doc;
+
+  void onChanged() {
+    if (formField.onChanged != null) formField.onChanged!();
+    setState(() {});
+  }
+
+  Widget fieldBuilder(BuildContext context) {
+    if (field.readOnly == true) return field.readOnlyField(doc: doc);
+    return field.formField(onChanged: onChanged, doc: doc);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return fieldBuilder(context);
+  }
+}
