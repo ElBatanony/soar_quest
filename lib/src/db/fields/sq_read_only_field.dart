2383034d5963af814e4e258df574b6df254c1a3a
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-05 18:43:05 +0300

    Update ReadOnlyField display value as String only

--- lib/src/db/fields/sq_read_only_field.dart
+++ lib/src/db/fields/sq_read_only_field.dart
@@ -12,9 +12,6 @@ class SQReadOnlyFormField extends DocFormField {
 class _ReadOnlyFormFieldState extends DocFormFieldState {
   @override
   Widget fieldBuilder(BuildContext context) {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      children: [Text(field.name), Text(": "), Text(field.value.toString())],
-    );
+    return Text(field.value.toString());
   }
 }
