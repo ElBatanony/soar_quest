26c69ea22b49699621d604479d9d41f43cd28950
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-05 23:37:48 +0300

    Hide SQVirtualField in DocCreateScreen

--- lib/src/screens/form_screens/doc_create_screen.dart
+++ lib/src/screens/form_screens/doc_create_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../db/sq_collection.dart';
 import '../form_screen.dart';
+import '../../db/fields/sq_virtual_field.dart';
 
 Future createDoc(SQDoc doc, BuildContext context) async {
   return doc.collection.createDoc(doc);
@@ -12,7 +13,7 @@ DocFormScreen docCreateScreen(
   String? title,
   List<SQField> initialFields = const [],
   List<String>? shownFields,
-  List<String> hiddenFields = const [],
+  List<String>? hiddenFields,
   String submitButtonText = "Create",
 }) {
   title ??= "Create ${collection.singleDocName}";
@@ -20,6 +21,10 @@ DocFormScreen docCreateScreen(
   SQDoc newDoc = collection.newDoc(initialFields: initialFields);
   newDoc.initialized = true;
 
+  hiddenFields ??= [];
+
+  hiddenFields.addAll(
+      newDoc.fields.whereType<SQVirtualField>().map((field) => field.name));
   return DocFormScreen(
     newDoc,
     submitFunction: createDoc,
