9afc85921973a749c7de914a45efd9b469dd0048
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-06 18:15:11 +0300

    Remoe TODO to use String instead of SQRef

--- lib/src/db/fields/sq_ref_field.dart
+++ lib/src/db/fields/sq_ref_field.dart
@@ -25,7 +25,6 @@ class SQRefField extends SQField<SQRef> {
     return SQRef.parse(source);
   }
 
-  // TODO: make value string
   @override
   Map<String, dynamic> collectField() {
     if (value == null) return {};
