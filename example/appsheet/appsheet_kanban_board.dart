7d8840cba75902852754cc994d4a88a3f3854076
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-10 20:07:44 +0300

    Create AppSheet Kanban Board example app

---
+++ example/appsheet/appsheet_kanban_board.dart
@@ -0,0 +1,49 @@
+import 'package:soar_quest/soar_quest.dart';
+
+import '../firebase_options.dart';
+
+late SQCollection workstreams, projects, tasks;
+
+void main() async {
+  List<SQField> userDocFields = [
+    SQStringField("Name"),
+    SQStringField("Role"),
+    SQImageField("Photo"),
+  ];
+
+  await SQApp.init("Kanban Board",
+      userDocFields: userDocFields,
+      firebaseOptions: DefaultFirebaseOptions.currentPlatform);
+
+  workstreams = FirestoreCollection(id: "Workstreams", fields: [
+    SQStringField("Workstream"),
+    SQStringField("test readonly", readOnly: true, value: "hamada"),
+    SQEnumField(SQStringField("Color"),
+        options: ["#4285F4", "#DB4437", "#F4B400", "#0F9D58"]),
+    SQRefDocsField("Related Projects",
+        refCollection: () => projects, refFieldName: "Workstream"),
+  ]);
+
+  projects = FirestoreCollection(id: "Projects", fields: [
+    SQStringField("Project"),
+    SQRefField("Workstream", collection: workstreams),
+    SQEnumField(SQStringField("Status"),
+        options: ["Not Started", "In Progress", "Complete"]),
+    SQRefDocsField("Tasks",
+        refCollection: () => tasks, refFieldName: "Project"),
+  ]);
+
+  tasks = FirestoreCollection(id: "Tasks", fields: [
+    SQStringField("Task"),
+    SQRefField("Project", collection: projects),
+    SQEnumField(SQStringField("Status"), options: ["To-do", "Complete"]),
+    SQUserRefField("Owner"),
+  ]);
+
+  SQApp.run(MainScreen([
+    CollectionScreen(collection: workstreams),
+    CollectionScreen(collection: projects),
+    CollectionScreen(collection: tasks),
+    ProfileScreen(),
+  ]));
+}
