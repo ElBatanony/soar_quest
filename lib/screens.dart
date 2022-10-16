7720eeb88bac978066fc4749d8aca3c41e5f10f2
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-16 11:38:23 +0300

    Delete MenuScreen

--- lib/screens.dart
+++ lib/screens.dart
@@ -2,7 +2,6 @@ export 'src/screens/screen.dart';
 export 'src/screens/collection_screen.dart';
 export 'src/screens/doc_screen.dart';
 export 'src/screens/form_screen.dart';
-export 'src/screens/menu_screen.dart';
 export 'src/screens/profile_screen.dart';
 export 'src/screens/main_screen.dart';
 
