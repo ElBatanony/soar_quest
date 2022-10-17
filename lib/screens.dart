4f71102bcb959ca1d1e98a3614205259f9bbc2fb
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-17 13:53:23 +0300

    Delete MainScreen

--- lib/screens.dart
+++ lib/screens.dart
@@ -3,7 +3,6 @@ export 'src/screens/collection_screen.dart';
 export 'src/screens/doc_screen.dart';
 export 'src/screens/form_screen.dart';
 export 'src/screens/profile_screen.dart';
-export 'src/screens/main_screen.dart';
 
 export 'src/screens/collection_screens/category_select_screen.dart';
 export 'src/screens/collection_screens/collection_filter_screen.dart';
