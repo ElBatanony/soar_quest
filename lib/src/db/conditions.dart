fb665a26125f19b5b5506c582a0c2f4bc031dda9
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-21 20:01:28 +0300

    Create DocContextCondition

--- lib/src/db/conditions.dart
+++ lib/src/db/conditions.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
 import 'sq_doc.dart';
 
 typedef DocCondition = bool Function(SQDoc doc);
+typedef DocContextCondition = bool Function(SQDoc doc, BuildContext context);
 
 bool trueCond(SQDoc doc) => true;
 bool falseCond(SQDoc doc) => false;
+
+bool trueContextCond(SQDoc doc, BuildContext context) => true;
