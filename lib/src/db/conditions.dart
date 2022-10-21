d52eeca69bbe3c92f582216a248b99c6de4edaf4
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-21 20:24:18 +0300

    Create inFormScreen DocContextCondition

--- lib/src/db/conditions.dart
+++ lib/src/db/conditions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/form_screen.dart';
+import '../screens/screen.dart';
 import 'sq_doc.dart';
 
 typedef DocCondition = bool Function(SQDoc doc);
@@ -9,3 +11,6 @@ bool trueCond(SQDoc doc) => true;
 bool falseCond(SQDoc doc) => false;
 
 bool trueContextCond(SQDoc doc, BuildContext context) => true;
+
+DocContextCondition inFormScreen =
+    (_, context) => ScreenState.of(context) is FormScreenState;
