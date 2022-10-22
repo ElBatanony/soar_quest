d90548bf4ca73142e8086c9f9c0819c92b75584b
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-22 12:04:24 +0300

    Remove DocCondition

--- lib/src/db/conditions.dart
+++ lib/src/db/conditions.dart
@@ -6,11 +6,7 @@ import '../screens/screen.dart';
 import '../ui/signed_in_content.dart';
 import 'sq_doc.dart';
 
-typedef DocCondition = bool Function(SQDoc doc);
 typedef DocContextCondition = bool Function(SQDoc doc, BuildContext context);
-
-bool trueCond(SQDoc doc) => true;
-bool falseCond(SQDoc doc) => false;
 
 bool trueContextCond(SQDoc doc, BuildContext context) => true;
 
