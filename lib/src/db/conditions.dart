4ab8fe1324412b10334d1830fd5fa3199d175557
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-22 11:33:57 +0300

    Add isSignedIn DocContextCondition

--- lib/src/db/conditions.dart
+++ lib/src/db/conditions.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../auth/sq_auth.dart';
 import '../screens/form_screen.dart';
 import '../screens/screen.dart';
+import '../ui/signed_in_content.dart';
 import 'sq_doc.dart';
 
 typedef DocCondition = bool Function(SQDoc doc);
@@ -14,3 +16,6 @@ bool trueContextCond(SQDoc doc, BuildContext context) => true;
 
 DocContextCondition inFormScreen =
     (_, context) => ScreenState.of(context) is FormScreenState;
+
+DocContextCondition isSignedIn =
+    (_, context) => SQAuth.user is SignedInUser && !SQAuth.user.isAnonymous;
