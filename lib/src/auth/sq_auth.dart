e15753ad484d8b2d1fddb60482b9c08f7a5f377d
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-22 11:53:35 +0300

    Add Sign In and Out Actions to userCollection

--- lib/src/auth/sq_auth.dart
+++ lib/src/auth/sq_auth.dart
@@ -1,7 +1,9 @@
 import '../db/fields/sq_string_field.dart';
 import '../db/firestore_collection.dart';
+import '../db/sq_action.dart';
 import '../db/sq_collection.dart';
 
+import '../screens/screen.dart';
 import 'sq_auth_manager.dart';
 import 'firebase_auth_manager.dart';
 import 'user_data.dart';
@@ -20,11 +22,14 @@ class SQAuth {
     SQAuth.userDocFields = userDocFields ?? [SQStringField("Full Name")];
     SQAuth.auth = authManager ?? FirebaseAuthManager();
     usersCollection = FirestoreCollection(
-      id: "Users",
-      fields: SQAuth.userDocFields,
-      singleDocName: "Profile Info",
-      deletes: false,
-    );
+          CustomAction("Sign Out", customExecute: (doc, context) async {
+            await SQAuth.auth.signOut();
+            ScreenState.of(context).refreshScreen();
+          }),
+          // TODO: add condition for showing sign in
+          GoScreenAction("Sign In",
+              screen: (doc) => SQAuth.auth.signInScreen(forceSignIn: true))
+        ]);
     await auth.init();
   }
 }
