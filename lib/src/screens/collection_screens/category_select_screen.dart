cecf68d1fc3f3277fd8cd269e0f74914f809769b
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-25 22:10:33 +0300

    Simplify CategorySelectScreen

--- lib/src/screens/collection_screens/category_select_screen.dart
+++ lib/src/screens/collection_screens/category_select_screen.dart
@@ -1,53 +1,35 @@
 import 'package:flutter/material.dart';
 
-import '../../db/sq_collection.dart';
+import '../../db/collection_slice.dart';
 import '../../db/fields/sq_ref_field.dart';
-import '../../ui/sq_button.dart';
-
+import '../../db/sq_collection.dart';
 import '../collection_screen.dart';
-import 'collection_filter_screen.dart';
+import '../screen.dart';
+import 'gallery_screen.dart';
 
-class CategorySelectScreen extends CollectionScreen {
+class CategorySelectScreen extends GalleryScreen {
   final SQRefField categoryField;
 
-  CategorySelectScreen(
-      {super.title,
-      required super.collection,
-      required this.categoryField,
-      super.docScreen,
-      super.key});
+  CategorySelectScreen({
+    super.title,
+    required super.collection,
+    required this.categoryField,
+  });
 
   @override
   State<CategorySelectScreen> createState() => _CategorySelectScreenState();
 }
 
 class _CategorySelectScreenState
-    extends CollectionScreenState<CategorySelectScreen> {
+    extends GalleryScreenState<CategorySelectScreen> {
   @override
   SQCollection<SQDoc> get collection => widget.categoryField.collection;
 
   @override
-  Widget docDisplay(SQDoc doc, BuildContext context) {
-    return SQButton(
-      doc.label,
-      onPressed: () {
-        SQRefField categoryFieldCopy = widget.categoryField.copy();
-        categoryFieldCopy.value = SQRef(
-          collectionPath: widget.categoryField.collection.path,
-          docId: doc.id,
-          label: doc.label,
-        );
-        print(categoryFieldCopy.name);
-        print(categoryFieldCopy.value);
-        CollectionFilter filter =
-            DocRefFieldFilter(docRefField: categoryFieldCopy);
-        CollectionFilterScreen(
-                title: "Category of",
-                collection: widget.collection,
-                filters: [filter],
-                docScreen: widget.docScreen)
-            .go(context);
-      },
-    );
+  Screen docScreen(SQDoc doc) {
+    final slice = CollectionSlice(widget.collection,
+        filter: DocRefFilter(widget.categoryField.name, doc.ref));
+    return CollectionScreen(
+        title: "${doc.label} ${widget.collection.id}", collection: slice);
   }
 }
