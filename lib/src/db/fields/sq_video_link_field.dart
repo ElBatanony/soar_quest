79759300b0c58f9f12e827b748199661ab91d025
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-25 22:14:18 +0300

    Add VideoLinkFormField

--- lib/src/db/fields/sq_video_link_field.dart
+++ lib/src/db/fields/sq_video_link_field.dart
@@ -1,4 +1,9 @@
-import 'package:soar_quest/src/db/fields/sq_string_field.dart';
+import 'package:flutter/material.dart';
+
+import '../../screens/collection_screens/video_screen.dart';
+import '../sq_doc.dart';
+import 'sq_string_field.dart';
+import 'sq_text_field.dart';
 
 class VideoLinkField extends SQStringField {
   VideoLinkField(String name, {String? url, super.editable})
@@ -12,4 +17,30 @@ class VideoLinkField extends SQStringField {
 
   @override
   VideoLinkField copy() => VideoLinkField(name, url: value, editable: editable);
+
+  @override
+  formField({Function? onChanged, SQDoc? doc}) {
+    return _VideoLinkFormField(this, onChanged: onChanged, doc: doc);
+  }
+}
+
+class _VideoLinkFormField extends SQFormField<VideoLinkField> {
+  const _VideoLinkFormField(super.field,
+      {required super.onChanged, required super.doc});
+
+  @override
+  createState() => __VideoLinkFormFieldState();
+}
+
+class __VideoLinkFormFieldState extends SQFormFieldState<VideoLinkField> {
+  @override
+  Widget readOnlyBuilder(BuildContext context) {
+    if (formField.doc == null) return super.readOnlyBuilder(context);
+    return VideoDocDisplay(formField.doc!, videoField: field);
+  }
+
+  @override
+  Widget fieldBuilder(BuildContext context) {
+    return SQTextField(formField, textParse: (text) => text);
+  }
 }
