9080b70073e1f078f985543bc121d2f2d4e99a01
ElBatanony <ahmedelbatanony@hotmail.com>
2022-10-25 21:04:41 +0300

    Simplify favourites feature

--- lib/src/features/favourites/favourites.dart
+++ lib/src/features/favourites/favourites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soar_quest/screens.dart';
 
 import '../../auth/sq_auth.dart';
 import '../../db/conditions.dart';
@@ -6,41 +7,15 @@ import '../../db/local_collection.dart';
 import '../../db/sq_action.dart';
 import '../../db/sq_collection.dart';
 import '../../db/fields/sq_ref_field.dart';
-import '../../screens/collection_screen.dart';
-import '../../ui/snackbar.dart';
-import '../../ui/sq_button.dart';
-
-class FavDoc extends SQDoc {
-  SQRef? favedDocRef;
-
-  FavDoc(super.id, {required this.favedDocRef, required super.collection});
-
-  FavDoc.fromDoc(SQDoc doc)
-      : this(doc.id,
-            favedDocRef: doc.value<SQRef>('ref'), collection: doc.collection);
-}
-
-class FavouritesCollection extends LocalCollection {
-  List<FavDoc> favDocs = [];
-
-  FavouritesCollection({required super.id, required super.fields})
-      : super(parentDoc: SQAuth.userDoc);
-
-  @override
-  Future<void> loadCollection() async {
-    await super.loadCollection();
-    favDocs = docs.map((doc) => FavDoc.fromDoc(doc)).toList();
-  }
-}
 
 class FavouritesFeature {
   SQCollection collection;
-  late final FavouritesCollection favouritesCollection;
+  late final SQCollection favouritesCollection;
 
   SQAction addToFavouritesAction() => CustomAction(
         "Fav",
         icon: Icons.favorite,
-        show: isInFavourites().not(),
+        show: isInFavourites().not,
         customExecute: (doc, context) async {
           var newFavDoc = SQDoc(doc.id, collection: favouritesCollection);
           newFavDoc.getField<SQRefField>("ref")!.value = doc.ref;
@@ -48,19 +23,26 @@ class FavouritesFeature {
         },
       );
 
-  SQAction removeFromFavouritesAction() => CustomAction("UnFav",
-          icon: Icons.delete_forever_sharp,
-          show: isInFavourites(), customExecute: (doc, context) async {
-        favouritesCollection.deleteDoc(doc);
-      });
+  SQAction removeFromFavouritesAction() => CustomAction(
+        "UnFav",
+        icon: Icons.delete_forever_sharp,
+        show: isInFavourites(),
+        customExecute: (doc, context) async {
+          favouritesCollection.deleteDoc(doc);
+        },
+      );
 
   FavouritesFeature({required this.collection}) {
-    favouritesCollection = FavouritesCollection(
-      id: "Favourite ${collection.id}",
-      fields: [
-        SQRefField("ref", collection: collection),
-      ],
-    );
+    favouritesCollection = LocalCollection(
+        id: "Favourite ${collection.id}",
+        fields: [SQRefField("ref", collection: collection)],
+        actions: [
+          GoScreenAction("",
+              screen: (doc) =>
+                  DocScreen(collection.getDoc(doc.value<SQRef>("ref")!.docId)!))
+        ],
+        parentDoc: SQAuth.userDoc,
+        readOnly: true);
     collection.actions
         .addAll([addToFavouritesAction(), removeFromFavouritesAction()]);
     favouritesCollection.loadCollection();
@@ -77,27 +59,4 @@ class FavouritesScreen extends CollectionScreen {
       super.isInline,
       super.key})
       : super(collection: favouritesFeature.favouritesCollection);
-
-  @override
-  State<FavouritesScreen> createState() => _FavouritesScreenState();
-}
-
-class _FavouritesScreenState extends CollectionScreenState<FavouritesScreen> {
-  @override
-  Widget docDisplay(SQDoc doc, BuildContext context) {
-    SQRef? originalDocRef = doc.value<SQRef>("ref");
-    return ListTile(
-      title: SQButton(doc.label,
-          onPressed: () async => originalDocRef == null
-              ? showSnackBar("Doc Ref is Null", context: context)
-              : widget.docScreen(await originalDocRef.doc()).go(context)),
-    );
-  }
-
-  @override
-  Widget screenBody(BuildContext context) {
-    return docs.isEmpty
-        ? Center(child: Text("Your favourites list is empty"))
-        : super.screenBody(context);
-  }
 }
