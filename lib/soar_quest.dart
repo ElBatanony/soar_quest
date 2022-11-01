e7657cb4eab01af5723577843490fbfbcafaecf6
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-01 08:35:08 +0300

    Move features export to soar_quest.dart

--- lib/soar_quest.dart
+++ lib/soar_quest.dart
@@ -1,7 +1,8 @@
 export 'src/sq_app.dart';
 export 'src/sq_auth.dart';
 export 'db.dart';
-export 'features.dart';
 export 'screens.dart';
 export 'storage.dart';
 export 'ui.dart';
+
+export 'src/features/favourites.dart';
