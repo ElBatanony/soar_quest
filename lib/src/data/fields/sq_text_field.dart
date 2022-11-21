2f2fc9574d174a546d03c4ca916b33db71c48f45
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-21 09:20:52 +0300

    Add maxLines param to SQTextField

--- lib/src/data/fields/sq_text_field.dart
+++ lib/src/data/fields/sq_text_field.dart
@@ -5,8 +5,10 @@ import '../sq_field.dart';
 class SQTextField<T> extends StatefulWidget {
   final SQFormField<SQField<T>> formField;
   final T Function(String) textParse;
+  final int maxLines;
 
-  const SQTextField(this.formField, {required this.textParse});
+  const SQTextField(this.formField,
+      {required this.textParse, this.maxLines = 1});
 
   @override
   SQTextFieldState createState() => SQTextFieldState();
@@ -32,6 +34,7 @@ class SQTextFieldState extends State<SQTextField<dynamic>> {
   Widget build(BuildContext context) {
     return TextField(
       controller: fieldTextController,
+      maxLines: widget.maxLines,
       onChanged: (text) {
         widget.formField.field.value = widget.textParse(text);
         callOnChange();
