807243c65e5038e7a87661d7c39efb15acf0c4d9
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-21 10:42:44 +0300

    Add Screen.signedIn param

--- lib/src/screens/screen.dart
+++ lib/src/screens/screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../sq_app.dart';
+import '../sq_auth.dart';
+import '../ui/sq_button.dart';
 import '../ui/sq_navbar.dart';
 
 Future<T?> _goToScreen<T>(
@@ -29,12 +31,14 @@ class Screen extends StatefulWidget {
   final IconData? icon;
   final bool isInline;
   final bool Function(BuildContext) show;
+  final bool signedIn;
 
   const Screen({
     required this.title,
     this.isInline = false,
     this.icon = Icons.stay_current_landscape,
     this.show = alwaysShowScreen,
+    this.signedIn = false,
   });
 
   @override
@@ -66,7 +70,7 @@ class Screen extends StatefulWidget {
   FloatingActionButton? floatingActionButton(ScreenState screenState) => null;
 
   Widget? bottomNavBar(ScreenState screenState) {
-    if (SQApp.navbarScreens.length > 1) return SQNavBar(SQApp.navbarScreens);
+    if (SQApp.navbarScreens.length >= 2) return SQNavBar(SQApp.navbarScreens);
     return null;
   }
 }
@@ -76,6 +80,26 @@ class ScreenState<T extends Screen> extends State<T> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.signedIn && SQAuth.isSignedIn == false) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        drawer: SQApp.drawer,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("You should be signed in to view this screen."),
+              SQButton("Sign In",
+                  onPressed: () => SQProfileScreen().go(context))
+            ],
+          ),
+        ),
+        bottomNavigationBar: widget.bottomNavBar(this),
+      );
+    }
+
     final Widget body = Builder(builder: (_) {
       return Container(
         padding: widget.isInline ? null : EdgeInsets.all(16.0),
