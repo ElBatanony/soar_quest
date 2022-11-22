e2c58c767133ca2f79812fc2ad147478cdd6f970
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-22 08:37:10 +0300

    Create MenuScreen

--- lib/soar_quest.dart
+++ lib/soar_quest.dart
@@ -30,6 +30,7 @@ export 'src/screens/collection_screen.dart';
 export 'src/screens/doc_screen.dart';
 export 'src/screens/form_screen.dart';
 export 'src/screens/tabs_screen.dart';
+export 'src/screens/menu_screen.dart';
 
 export 'src/screens/collection_screens/category_select_screen.dart';
 export 'src/screens/collection_screens/collection_filter_screen.dart';
---
+++ lib/src/screens/menu_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../ui/sq_button.dart';
+import 'screen.dart';
+
+class MenuScreen extends Screen {
+  final List<Screen> screens;
+
+  const MenuScreen({required this.screens, required super.title});
+
+  @override
+  Widget screenBody(ScreenState<Screen> screenState) {
+    return ListView(
+      children: [
+        for (final screen in screens)
+          SQButton(
+            screen.title,
+            onPressed: () => screen.go(screenState.context),
+          ),
+      ],
+    );
+  }
+}
