c2ef76b70e216017db181211f0653d8d132fb487
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-22 07:34:48 +0300

    Make SQNavBar stateless

--- lib/src/ui/sq_navbar.dart
+++ lib/src/ui/sq_navbar.dart
@@ -3,26 +3,14 @@ import 'package:flutter/material.dart';
 import '../sq_app.dart';
 import '../screens/screen.dart';
 
-class SQNavBar extends StatefulWidget {
+class SQNavBar extends StatelessWidget {
   final List<Screen> screens;
+  final int initialIndex;
 
-  const SQNavBar(this.screens);
+  List<Screen> visibleScreens(BuildContext context) =>
+      screens.where((screen) => screen.show(context)).toList();
 
-  @override
-  State<SQNavBar> createState() => _SQNavBarState();
-}
-
-class _SQNavBarState extends State<SQNavBar> {
-  late int initialIndex;
-
-  List<Screen> get screens =>
-      widget.screens.where((screen) => screen.show(context)).toList();
-
-  @override
-  void initState() {
-    initialIndex = SQApp.selectedNavScreen;
-    super.initState();
-  }
+  SQNavBar(this.screens) : initialIndex = SQApp.selectedNavScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +18,12 @@ class _SQNavBarState extends State<SQNavBar> {
     return NavigationBar(
       onDestinationSelected: (int index) {
         SQApp.selectedNavScreen = index;
-        screens[SQApp.selectedNavScreen].go(context, replace: true);
+        visibleScreens(context)[SQApp.selectedNavScreen]
+            .go(context, replace: true);
       },
       selectedIndex: initialIndex,
       surfaceTintColor: Colors.white,
-      destinations: screens
+      destinations: visibleScreens(context)
           .map((screen) => NavigationDestination(
               icon: Icon(screen.icon), label: screen.title))
           .toList(),
