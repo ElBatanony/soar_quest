2dd34ee8f86b5fa83b15027fd6ff732beda0a284
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-25 00:26:57 +0300

    Make SocialFeed unfeature as a TelegramScreen

--- lib/src/unfeatures/social_feed.dart
+++ lib/src/unfeatures/social_feed.dart
@@ -1,29 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher_string.dart';
+import '../screens/telegram_screen.dart';
 
-import '../screens/screen.dart';
-import '../ui/sq_button.dart';
-
-class SocialFeed extends Screen {
-  const SocialFeed({required super.title, super.icon});
-
-  @override
-  Widget screenBody(ScreenState<Screen> screenState) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "Soar Quest does not promote social feeds (posts, likes, etc.). "
-          "We recommend using tools such as Telegram Channels for such purposes.",
-          textAlign: TextAlign.center,
-          style: Theme.of(screenState.context).textTheme.bodyLarge,
-        ),
-        SQButton(
-          "View Telegram Channels",
-          onPressed: () async => launchUrlString("https://tlgrm.eu/channels",
-              mode: LaunchMode.externalApplication),
-        ),
-      ],
-    );
-  }
+class SocialFeed extends TelegramScreen {
+  const SocialFeed({required super.title, super.icon})
+      : super(
+          message:
+              "Soar Quest does not promote social feeds (posts, likes, etc.). "
+              "We recommend using tools such as Telegram Channels for such purposes.",
+          telegramUsername: "",
+          buttonText: "Checkout Telegram",
+        );
 }
