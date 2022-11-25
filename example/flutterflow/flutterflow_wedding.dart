db6cee8a8eb6ce49ac7fe090e017b4847df346ee
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-26 01:42:35 +0300

    Begin FlutterFlow Wedding example app

---
+++ example/flutterflow/flutterflow_wedding.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:soar_quest/soar_quest.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  await SQApp.init("Wedding App",
+      firebaseOptions: DefaultFirebaseOptions.currentPlatform);
+
+  // TODO: guest list
+  // TODO: transportation page
+  // TODO: wiki pages (use flutter_quill). maybe as a field (not page)
+  // TODO: add maps (use flutter_map)
+
+  SQApp.run(
+    [
+      MenuScreen(screens: [
+        Screen(title: "Transportation"),
+        Screen(title: "Ceremony"),
+        Screen(title: "Our Story"),
+        TabsScreen(title: "Wedding Party", screens: const [
+          Screen(title: "Bridesmaids", isInline: true),
+          Screen(title: "Groomsmen", isInline: true),
+        ])
+      ], title: "Home"),
+      SocialFeed(title: "Social Feed", icon: Icons.no_drinks),
+    ],
+    drawer: SQDrawer(const [FeedbackScreen(telegramUsername: "batanony")]),
+  );
+}
