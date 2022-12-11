1525e3d08754256f9abc55d13c8ff1ac0b120778
ElBatanony <ahmedelbatanony@hotmail.com>
2022-12-11 17:19:55 +0300

    Add SQButton padding param

--- lib/src/ui/sq_button.dart
+++ lib/src/ui/sq_button.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class SQButton extends StatelessWidget {
-  const SQButton(this.text, {required this.onPressed})
-      : icon = null,
+  const SQButton(
+    this.text, {
+    required this.onPressed,
+    this.padding = 3,
+  })  : icon = null,
         iconSize = 24;
 
   const SQButton.icon(this.icon,
-      {required this.onPressed, this.text, this.iconSize = 24});
+      {required this.onPressed,
+      this.text,
+      this.iconSize = 24,
+      this.padding = 3});
 
   final String? text;
   final void Function()? onPressed;
   final IconData? icon;
   final double iconSize;
+  final double padding;
 
   @override
   Widget build(BuildContext context) => Container(
-        padding: const EdgeInsets.all(3),
+        padding: EdgeInsets.all(padding),
         child: icon == null
             ? ElevatedButton(onPressed: onPressed, child: Text(text!))
             : text == null
