2ae59e48b283cf64f39bbdd5abc2dc822a3d2c5b
ElBatanony <ahmedelbatanony@hotmail.com>
2022-12-11 17:11:42 +0300

    Handle SQTimestamp parsing itself

--- lib/src/data/types/sq_timestamp.dart
+++ lib/src/data/types/sq_timestamp.dart
@@ -18,6 +18,7 @@ class SQTimestamp extends Timestamp {
 
   static SQTimestamp? parse(dynamic source) {
     if (source == null) return null;
+    if (source is SQTimestamp) return source;
     if (source is Timestamp) return SQTimestamp.fromTimestamp(source);
     if (source is Map<String, dynamic> && source['_seconds'] is int)
       return SQTimestamp(source['_seconds'] as int, 0);
