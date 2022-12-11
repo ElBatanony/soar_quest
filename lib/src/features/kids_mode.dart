d7662040d2e1b9b6ab4582dcce0b31d395d3cc1a
ElBatanony <ahmedelbatanony@hotmail.com>
2022-12-11 17:27:43 +0300

    Add Kids Mode feature

---
+++ lib/src/features/kids_mode.dart
@@ -0,0 +1,39 @@
+import '../data/collections/collection_slice.dart';
+import '../data/fields/sq_enum_field.dart';
+import '../data/fields/sq_int_field.dart';
+import '../data/fields/sq_timestamp_field.dart';
+import '../data/user_settings.dart';
+
+const dateOfBirthFieldName = 'Date of Birth';
+const maturityLevelFieldName = 'Maturity Level';
+
+class SQDateOfBirthField extends SQTimestampField {
+  SQDateOfBirthField() : super(dateOfBirthFieldName);
+}
+
+class SQMaturityRatingField extends SQEnumField<int> {
+  SQMaturityRatingField()
+      : super(SQIntField(maturityLevelFieldName, defaultValue: 0),
+            options: [0, 6, 9, 12, 14, 16, 18]);
+}
+
+class KidsModeFilter extends CollectionFilter {
+  @override
+  List<SQDoc> filter(List<SQDoc> docs) {
+    final dateOfBirth = UserSettings()
+            .getSetting<SQTimestamp>(dateOfBirthFieldName)
+            ?.toDate() ??
+        DateTime(1990);
+
+    final userAge = DateTime.now().difference(dateOfBirth).inDays ~/ 365;
+
+    return docs
+        .where((doc) =>
+            (doc.getValue<int>(maturityLevelFieldName) ?? 0) <= userAge)
+        .toList();
+  }
+}
+
+class KidsModeSlice extends CollectionSlice {
+  KidsModeSlice(super.collection) : super(filter: KidsModeFilter());
+}
