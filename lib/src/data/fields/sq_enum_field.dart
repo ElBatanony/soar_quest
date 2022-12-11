a8ad36bb512670ba5fd3b5240dd5cba0c2918b05
ElBatanony <ahmedelbatanony@hotmail.com>
2022-12-11 14:11:23 +0300

    Set SQEnumField subfield to inline (vs fieldLabel)

--- lib/src/data/fields/sq_enum_field.dart
+++ lib/src/data/fields/sq_enum_field.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-import '../../screens/form_screen.dart';
 import '../../ui/sq_button.dart';
 import '../sq_doc.dart';
 
 class SQEnumField<T> extends SQField<T> {
   SQEnumField(this.subfield, {required this.options, super.show})
       : super(subfield.name, defaultValue: subfield.defaultValue) {
-    subfield.editable = false;
+    subfield
+      ..editable = false
+      ..isInline = true;
   }
 
   SQField<T> subfield;
@@ -22,11 +23,6 @@ class SQEnumField<T> extends SQField<T> {
 
 class _SQEnumFormField<T> extends SQFormField<T, SQEnumField<T>> {
   const _SQEnumFormField(super.field, super.docScreenState);
-
-  @override
-  Widget fieldLabel(screenState) => screenState is FormScreenState
-      ? super.fieldLabel(screenState)
-      : Container();
 
   @override
   Widget readOnlyBuilder(context) => field.subfield.formField(docScreenState);
