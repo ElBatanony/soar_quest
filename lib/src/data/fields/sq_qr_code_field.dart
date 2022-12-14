ff838e32e28ffd65f3485caa57b366dcd546f6b3
ElBatanony <ahmedelbatanony@hotmail.com>
2022-12-14 22:14:56 +0300

    Add QR Code scanning to SQQRCodeField

--- lib/src/data/fields/sq_qr_code_field.dart
+++ lib/src/data/fields/sq_qr_code_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../../ui/sq_button.dart';
 import '../../ui/sq_qr_code_display.dart';
@@ -17,20 +18,40 @@ class _SQQRCodeFormField extends SQFormField<String, SQQRCodeField> {
   const _SQQRCodeFormField(super.field, super.docScreenState);
 
   Future<void> displayQRCode(BuildContext context, String qrCodeString) =>
-      showDialog<void>(
+      showDialog(
         context: context,
         builder: (context) => AlertDialog(
           title: Text(field.name),
           content: SQQRCodeDisplay(string: qrCodeString, size: 200),
-          actions: [
-            TextButton(
-              style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge),
-              child: const Text('Close'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
+          actions: [_closeDialogButton(context)],
         ),
+      );
+
+  Future<void> scanQRCode(BuildContext context) => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Scan ${field.name}'),
+          content: SizedBox(
+            height: 350,
+            child: MobileScanner(
+              controller:
+                  MobileScannerController(formats: [BarcodeFormat.qrCode]),
+              onDetect: (capture) {
+                final newString = capture.barcodes.first.rawValue;
+                if (newString != null) setDocValue(newString);
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          actions: [_closeDialogButton(context)],
+        ),
+      );
+
+  TextButton _closeDialogButton(BuildContext context) => TextButton(
+        style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge),
+        child: const Text('Close'),
+        onPressed: () => Navigator.of(context).pop(),
       );
 
   @override
@@ -40,10 +61,13 @@ class _SQQRCodeFormField extends SQFormField<String, SQQRCodeField> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         super.fieldLabel(context),
-        if (stringValue != null)
+        if (stringValue != null && !isInFormScreen)
           SQButton.icon(Icons.qr_code_2,
               text: 'Display',
               onPressed: () async => displayQRCode(context, stringValue)),
+        if (isInFormScreen)
+          SQButton.icon(Icons.qr_code_2,
+              text: 'Scan', onPressed: () async => scanQRCode(context))
       ],
     );
   }
