d09040ad460b6fec60a49ccaa97bb8d18df8aaa1
ElBatanony <ahmedelbatanony@hotmail.com>
2023-02-08 08:00:06 +0300

    Create SQFirebaseAnalytics

--- lib/src/data/sq_analytics.dart
+++ lib/src/data/sq_analytics.dart
@@ -1,5 +1,21 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
 abstract class SQAnalytics {
   Future<void> logEvent(String name, {Map<String, dynamic>? params});
 
   void init();
+}
+
+class SQFirebaseAnalytics extends SQAnalytics {
+  @override
+  void init() {
+    _instance = FirebaseAnalytics.instance;
+  }
+
+  late FirebaseAnalytics _instance;
+
+  @override
+  Future<void> logEvent(String name, {Map<String, dynamic>? params}) async {
+    await _instance.logEvent(name: name);
+  }
 }
