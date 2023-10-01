4bc0e846cdba07d41caa4ebdaf0d45d012826577
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-01 17:07:12 +0300

    Create Mini App PopupParams class

--- lib/src/mini_apps/popups.dart
+++ lib/src/mini_apps/popups.dart
@@ -2,6 +2,37 @@ import 'js.dart';
 
 enum PopupButtonType { ok, close, cancel, destructive }
 
+class PopupParams {
+  PopupParams({
+    required this.message,
+    this.title,
+    this.buttons,
+  })  : assert(title == null || title.length < 64,
+            'Max 64 characters in popup title'),
+        assert(message.isNotEmpty && message.length <= 256,
+            'Popup messages must be 1-256 characters'),
+        assert(buttons == null || (buttons.isNotEmpty && buttons.length <= 3),
+            'Popup should have 1-3 buttons');
+
+  final String? title;
+  final String message;
+  final List<PopupButton>? buttons;
+
+  JsObject jsify() => JsObject.jsify({
+        'message': message,
+        'title': title,
+        if (buttons != null)
+          'buttons': [
+            for (final button in buttons!)
+              JsObject.jsify({
+                if (button.id != null) 'id': button.id,
+                'type': button.type?.name ?? 'default',
+                if (button.text != null) 'text': button.text
+              })
+          ],
+      });
+}
+
 class ScanQrPopupParams {
   ScanQrPopupParams(this.text)
       : assert(text == null || text.length < 64,
