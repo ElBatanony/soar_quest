6c4de57a5e29d40ca3afd57d6b2ee8d83e1846ba
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-01 16:49:11 +0300

    Export Material Color(s) from mini_apps/color.dart

--- lib/src/mini_apps/color.dart
+++ lib/src/mini_apps/color.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart' show Color, Colors;
 export 'package:flutter/material.dart' show Color;
 
 enum MiniAppColorScheme { light, dark }
