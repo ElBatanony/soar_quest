b7b16d22585a8960f74e31032084f31591270a15
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-01 15:34:35 +0300

    Create MiniAppColor class and MiniAppColorScheme

---
+++ lib/src/mini_apps/color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+export 'package:flutter/material.dart' show Color;
+
+enum MiniAppColorScheme { light, dark }
+
+extension MiniAppColorExtension on Color {
+  MiniAppColor toMiniAppColor() => MiniAppColor(toHex());
+  String toHex() => '#${value.toRadixString(16).padLeft(8, '0').substring(2)}';
+}
+
+Color _hexToColor(dynamic hexString) {
+  if (hexString is! String) return Colors.black;
+  final hex = hexString.toUpperCase().replaceAll('#', '');
+  return Color(int.parse('FF$hex', radix: 16));
+}
+
+class MiniAppColor extends Color {
+  MiniAppColor(dynamic hex) : super(_hexToColor(hex).value);
+
+  @override
+  String toString() => toHex();
+}
