215840edf6732d9f3c6a347597ef637778e9a721
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-01 17:06:35 +0300

    Add Mini App PopupButton class

--- lib/src/mini_apps/popups.dart
+++ lib/src/mini_apps/popups.dart
@@ -1,5 +1,7 @@
 import 'js.dart';
 
+enum PopupButtonType { ok, close, cancel, destructive }
+
 class ScanQrPopupParams {
   ScanQrPopupParams(this.text)
       : assert(text == null || text.length < 64,
@@ -8,4 +10,23 @@ class ScanQrPopupParams {
   final String? text;
 
   JsObject jsify() => JsObject.jsify({'text': text});
+}
+
+class PopupButton {
+  PopupButton({this.id, this.type, this.text, this.onPressed})
+      : assert(id == null || id.length < 64,
+            'Max 64 characters in popup button id'),
+        assert(text == null || text.length < 64,
+            'Max 64 characters in popup button text'),
+        assert(
+            (type != PopupButtonType.destructive && type != null) ||
+                text != null,
+            '''Popup button text is required if default or destructive button type'''),
+        assert(onPressed == null || id != null, 'onPressed requires an ID');
+
+  final String? id;
+  final PopupButtonType? type;
+  final String? text;
+
+  final void Function()? onPressed;
 }
