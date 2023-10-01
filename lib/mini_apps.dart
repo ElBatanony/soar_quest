a2b53fd7a2db80a1b4c3e88a8a573d59de50dc11
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-01 18:01:39 +0300

    Create mini_apps.dart barrel file

---
+++ lib/mini_apps.dart
@@ -0,0 +1,11 @@
+export 'src/mini_apps/back_button.dart';
+export 'src/mini_apps/cloud_storage.dart';
+export 'src/mini_apps/collection.dart';
+export 'src/mini_apps/color.dart';
+export 'src/mini_apps/init_data.dart';
+export 'src/mini_apps/main_button.dart';
+export 'src/mini_apps/mini_app.dart';
+export 'src/mini_apps/popups.dart';
+export 'src/mini_apps/testing_screen.dart';
+export 'src/mini_apps/theme_params.dart';
+export 'src/mini_apps/user.dart';
