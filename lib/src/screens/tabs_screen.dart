fc2eebfc802a3a593a15ebd539a2f11bda42a331
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-09 22:00:20 +0300

    Create TabsScreenWidget for TabsScreen

--- lib/src/screens/tabs_screen.dart
+++ lib/src/screens/tabs_screen.dart
@@ -63,3 +63,59 @@ class _StatefulTabState extends State<_StatefulTab>
       controller: widget.tabsScreen.tabController,
       children: widget.tabsScreen.screens.map((s) => s.toWidget()).toList());
 }
+
+class TabsScreenWidget extends StatefulWidget {
+  const TabsScreenWidget(this.tabsScreen, this.screens);
+
+  final TabsScreen tabsScreen;
+  final List<Screen> screens;
+
+  @override
+  State<TabsScreenWidget> createState() => _TabsScreenWidgetState();
+}
+
+class _TabsScreenWidgetState extends State<TabsScreenWidget>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: widget.screens.length, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(context) {
+    final tabBar = TabBar(
+      controller: tabController,
+      isScrollable: true,
+      onTap: (value) => tabController.animateTo(value),
+      tabs: widget.screens.map((screen) => Tab(text: screen.title)).toList(),
+    );
+    final PreferredSizeWidget appBar = PreferredSize(
+      preferredSize: Size.fromHeight(tabBar.preferredSize.height),
+      child: AppBar(
+        toolbarHeight: tabBar.preferredSize.height,
+        bottom: tabBar,
+        elevation: 0,
+      ),
+    );
+    return Scaffold(
+      appBar: appBar,
+      body: Padding(
+        padding: widget.tabsScreen.padding,
+        child: TabBarView(
+            controller: tabController,
+            children: widget.screens.map((s) => s.toWidget()).toList()),
+      ),
+      floatingActionButton: widget.tabsScreen.floatingActionButton(),
+      bottomNavigationBar: widget.tabsScreen.navigationBar(),
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+}
