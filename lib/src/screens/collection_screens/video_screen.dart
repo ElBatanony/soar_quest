affb65ca94fe811a89b5463dcd63236e604d2826
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-09 17:22:18 +0300

    Delete VideoDocDisplay (DocScreen)

--- lib/src/screens/collection_screens/video_screen.dart
+++ lib/src/screens/collection_screens/video_screen.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
+import '../../data/sq_doc.dart';
 import '../../fields/video_link_field.dart';
 import '../collection_screen.dart';
-import '../doc_screen.dart';
 
 class VideoCollectionScreen extends CollectionScreen {
   VideoCollectionScreen({
@@ -18,41 +18,6 @@ class VideoCollectionScreen extends CollectionScreen {
 
   @override
   docDisplay(doc) => VideoDocDisplay(doc, videoField: videoField).toWidget();
-}
-
-class VideoDocDisplay extends DocScreen {
-  VideoDocDisplay(super.doc, {required this.videoField});
-
-  final SQVideoLinkField videoField;
-
-  YoutubePlayerController? _controller;
-
-  @override
-  void initScreen() {
-    super.initScreen();
-    final videoFieldValue = doc.getValue<String>(videoField.name);
-    if (videoFieldValue == null) return;
-    _controller = YoutubePlayerController(
-      params: const YoutubePlayerParams(showFullscreenButton: true),
-    );
-    unawaited(_controller?.loadVideo(videoFieldValue));
-  }
-
-  @override
-  void dispose() {
-    unawaited(_controller?.close());
-    super.dispose();
-  }
-
-  @override
-  Widget screenBody() => Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: _controller != null
-              ? YoutubePlayer(controller: _controller!)
-              : const Text('No video here'),
-        ),
-      );
 }
 
 class VideoDisplayWidget extends StatefulWidget {
