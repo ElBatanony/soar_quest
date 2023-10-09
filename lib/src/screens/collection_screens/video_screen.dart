c6f7d33ce821fde7b2f5b33c84e18769d7d4c60b
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-09 17:15:06 +0300

    Create VideoDisplayWidget

--- lib/src/screens/collection_screens/video_screen.dart
+++ lib/src/screens/collection_screens/video_screen.dart
@@ -54,3 +54,49 @@ class VideoDocDisplay extends DocScreen {
         ),
       );
 }
+
+class VideoDisplayWidget extends StatefulWidget {
+  const VideoDisplayWidget(this.doc, this.videoField);
+
+  final SQDoc doc;
+  final SQVideoLinkField videoField;
+
+  @override
+  State<VideoDisplayWidget> createState() => _VideoDisplayWidgetState();
+}
+
+class _VideoDisplayWidgetState extends State<VideoDisplayWidget> {
+  _VideoDisplayWidgetState();
+
+  YoutubePlayerController? _controller;
+
+  Future<void> loadVideo() async {
+    final videoFieldValue = widget.doc.getValue<String>(widget.videoField.name);
+    if (videoFieldValue == null) return;
+    _controller = YoutubePlayerController(
+        params: const YoutubePlayerParams(showFullscreenButton: true));
+    await _controller?.cueVideoById(videoId: videoFieldValue);
+  }
+
+  @override
+  void initState() {
+    unawaited(loadVideo());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: _controller != null
+              ? YoutubePlayer(controller: _controller!)
+              : const Text('No video here'),
+        ),
+      );
+
+  @override
+  void dispose() {
+    unawaited(_controller?.close());
+    super.dispose();
+  }
+}
