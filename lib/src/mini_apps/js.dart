78806645cb9b7b309b6bc5abbf0a760948fe94a5
ElBatanony <ahmedelbatanony@hotmail.com>
2023-10-10 20:02:25 +0300

    Refactor jsCallbackToFuture (one return arg)

--- lib/src/mini_apps/js.dart
+++ lib/src/mini_apps/js.dart
@@ -11,11 +11,11 @@ final webAppJsObject = (context['Telegram'] as JsObject)['WebApp'] as JsObject;
 Future<T> jsCallbackToFuture<T>(
     JsObject js, String methodName, List<dynamic> args) {
   final completer = Completer<T>();
-  final callback = JsFunction.withThis((_, dynamic arg1, dynamic arg2) {
-    if (arg2 == null) return completer.complete(arg1 as T);
-    return completer.complete(arg2 as T);
-  });
-  args.add(callback);
-  js.callMethod(methodName, args);
+  js.callMethod(methodName, [
+    ...args,
+    JsFunction.withThis((_, dynamic ret1) {
+      completer.complete(ret1 as T);
+    })
+  ]);
   return completer.future;
 }
